m0at/OpenRadioss

commit bd168cea5da14e5a95e6b24b95f8305ddb1f11d9

Starter optimization : rwall splitting is now done before ddsplit call, spmd_anim... computation is skipped if xfem is not used Domain decomposition : add the law 90 cost , update the law 25 cost, add the ISMSTR cost to the element cost Engine optimization : minor optimizations

starter/source/constraints/general/rwall/split_rwall.F90
@@ -1,0 +1,135 @@
+!Copyright>        OpenRadioss
+!Copyright>        Copyright (C) 1986-2024 Altair Engineering Inc.
+!Copyright>
+!Copyright>        This program is free software: you can redistribute it and/or modify
+!Copyright>        it under the terms of the GNU Affero General Public License as published by
+!Copyright>        the Free Software Foundation, either version 3 of the License, or
+!Copyright>        (at your option) any later version.
+!Copyright>
+!Copyright>        This program is distributed in the hope that it will be useful,
+!Copyright>        but WITHOUT ANY WARRANTY; without even the implied warranty of
+!Copyright>        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!Copyright>        GNU Affero General Public License for more details.
+!Copyright>
+!Copyright>        You should have received a copy of the GNU Affero General Public License
+!Copyright>        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+!Copyright>
+!Copyright>
+!Copyright>        Commercial Alternative: Altair Radioss Software
+!Copyright>
+!Copyright>        As an alternative to this open-source version, Altair also offers Altair Radioss
+!Copyright>        software under a commercial license.  Contact Altair to discuss further if the
+!Copyright>        commercial version may interest you: https://www.altair.com/radioss/.
+      module split_rwall_mod
+      contains
+! ======================================================================================================================
+!                                                   PROCEDURES
+! ======================================================================================================================
+!! \brief Here is a small description of the routine, [after the header]
+!! \details if needed, more details can be added here
+        subroutine split_rwall(nrwall,nspmd,nnprw ,slprw,nprw,lprw,constraint_struct)
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Modules
+! ----------------------------------------------------------------------------------------------------------------------
+          use constraint_mod , only : constraint_
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+          implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Included files
+! ----------------------------------------------------------------------------------------------------------------------
+#include "my_real.inc"
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Arguments
+! ----------------------------------------------------------------------------------------------------------------------
+          integer, intent(in) :: nrwall !< number of RWALL
+          integer, intent(in) :: nspmd !< number of processor
+          integer, intent(in) :: nnprw !< 2nd dim of nprw
+          integer, intent(in) :: slprw !< dim of lprw
+          integer, dimension(nrwall*nnprw), intent(in) :: nprw !<  rwall data
+          integer, dimension(slprw), intent(in) :: lprw !< list of S node 
+          type(constraint_), intent(inout) :: constraint_struct !< constraint structure for the splitting
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Local variables
+! ----------------------------------------------------------------------------------------------------------------------
+          integer :: i,n,j
+          integer :: index_lprw,index_m_node
+          integer :: s_node_number,proc_number,max_node_number
+          integer :: m_node_id,node_id,proc_id
+          integer :: proc_main
+          integer, dimension(nspmd) :: proc_list
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   External functions
+! ----------------------------------------------------------------------------------------------------------------------
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Body
+! ----------------------------------------------------------------------------------------------------------------------
+          constraint_struct%rwall%dd(1:nspmd+2,1:nrwall) = 0
+          index_lprw = 0 ! index for the S node
+          ! ------------
+          do n=1,nrwall
+            index_m_node = 2*nrwall + n ! index for the M node
+            m_node_id = nprw(index_m_node) ! get the M node id
+            s_node_number = nprw(n) ! get the number of S node id
+            constraint_struct%rwall%spmd(n)%m_proc_list(1:nspmd) = 0
+            if(m_node_id/=0) then
+              proc_number = 0
+              call plist_ifront(proc_list,m_node_id,proc_number) ! get the list of proce where M node is defined
+              ! ---------
+              ! save the list
+              do i =1,proc_number
+                proc_id = proc_list(i)
+                constraint_struct%rwall%spmd(n)%m_proc_list(proc_id) = 1
+              enddo
+              ! ---------
+
+              ! ---------            
+              do i=1,s_node_number
+                proc_number = 0
+                node_id = lprw(index_lprw+i)
+                call plist_ifront(proc_list,node_id,proc_number) ! get the list of proce where S node is defined
+                do j=1,proc_number
+                  proc_id = proc_list(j)
+                  constraint_struct%rwall%dd(proc_id,n) = constraint_struct%rwall%dd(proc_id,n) + 1
+                enddo
+              enddo
+              ! ---------              
+            endif
+        
+            ! ---------
+            ! get the main processor of the current rwall
+            proc_main = 1
+            max_node_number = 0
+            do i=1,nspmd
+              if(constraint_struct%rwall%dd(i,n)>max_node_number) then
+                max_node_number = constraint_struct%rwall%dd(i,n)
+                proc_main = i
+              elseif(constraint_struct%rwall%dd(i,n)==0.and.constraint_struct%rwall%spmd(n)%m_proc_list(i)==1) then
+                constraint_struct%rwall%dd(i,n) = -1
+                if(s_node_number==0) proc_main = i
+              endif
+            enddo
+            ! ---------
+ 
+
+            constraint_struct%rwall%dd(nspmd+1,n) = s_node_number
+            constraint_struct%rwall%dd(nspmd+2,n) = proc_main
+            ! ---------
+            do i=1,nspmd
+              constraint_struct%rwall%spmd(n)%pmain = 0
+              constraint_struct%rwall%spmd(n)%s_node_number = 0
+              if(constraint_struct%rwall%spmd(n)%m_proc_list(i)==1) then
+                constraint_struct%rwall%spmd(n)%pmain = proc_main
+                constraint_struct%rwall%spmd(n)%s_node_number = s_node_number
+              endif
+            enddo
+            ! ---------
+
+            index_lprw = index_lprw + s_node_number
+          enddo
+          ! ------------
+
+! ----------------------------------------------------------------------------------------------------------------------
+        end subroutine split_rwall
+      end module split_rwall_mod

common_source/includes/weights_p4linux964_spmd.inc
@@ -72,10 +72,15 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
         NLOCAL_OPTION(1:10) = 0.
 
 !     LAW 01
-       SOL1TNL(1,1) = .0000001139 !.0000001301    
-       SOL8TNL(1,1) = .0000002589 !.0000003212    
-       TET4TNL(1,1) = .0000000916 !.0000001229   
-       TET10TNL(1,1)= .0000003281 !.0000004662    
+       SOL1TNL(1,1) = .0000001139
+       SOL8TNL(1,1) = .0000002589 
+       TET4TNL(1,1) = .0000000916
+       TET10TNL(1,1)= .0000003281
+!     LAW 01 + ISMSTR = 10 or 12
+       SOL1TNL(1,2) = .0000002010
+       SOL8TNL(1,2) = .0000016080
+       TET4TNL(1,2) = .0000002010
+       TET10TNL(1,2)= .0000008040 
 !     LAW 02
        SOL1TNL(2,1) = .0000001235 !.0000001512    
        SOL8TNL(2,1) = .0000004478 !.0000006297   
@@ -132,10 +137,15 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
        TET4TNL(24,1) = .0000003826 !.0000004260
        TET10TNL(24,1)= .0000014510 !.0000020050
 !     LAW 25
-       SOL1TNL(25,1) = .0000002685 !.0000004227
-       SOL8TNL(25,1) = .0000020100 !.0000031570 
-       TET4TNL(25,1) = .0000002757 !.0000003445 
-       TET10TNL(25,1)= .0000011110 !.0000016050 
+       SOL1TNL(25,1) = .0000003084
+       SOL8TNL(25,1) = .0000024600
+       TET4TNL(25,1) = .0000003084
+       TET10TNL(25,1)= .0000012330
+!     LAW 25 Crasurv
+       SOL1TNL(25,2) = .0000003634
+       SOL8TNL(25,2) = .0000029070
+       TET4TNL(25,2) = .0000003634
+       TET10TNL(25,2)= .0000014570
 !     LAW 28
        SOL1TNL(28,1) = .0000002758 !.0000003664 
        SOL8TNL(28,1) = .0000016610 !.0000021385 
@@ -177,10 +187,15 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
        TET4TNL(36,3) = .0000002050 !.0000002854 
        TET10TNL(36,3)= .0000008800 !.0000010970 
 !     LAW 38
-       SOL1TNL(38,1) = .0000005909 !.0000008197   
-       SOL8TNL(38,1) = .0000039670 !.0000057300   
-       TET4TNL(38,1) = .0000005065 !.0000006935   
-       TET10TNL(38,1)= .0000020590 !.0000025160   
+       SOL1TNL(38,1) = .0000007220
+       SOL8TNL(38,1) = .0000053000
+       TET4TNL(38,1) = .0000007220
+       TET10TNL(38,1)= .0000028910
+!     LAW 38 + ISMSTR=10 or 12
+       SOL1TNL(38,2) = .0000007815
+       SOL8TNL(38,2) = .0000061000
+       TET4TNL(38,2) = .0000007815
+       TET10TNL(38,2)= .0000030900  
 !     LAW 40
        SOL1TNL(40,1) = .0000002842 !.0000004917   
        SOL8TNL(40,1) = .0000017380 !.0000030250   
@@ -206,10 +221,10 @@ C M = 2
        TET4TNL(42,3) = .0000005400 !.0000006823 
        TET10TNL(42,3)= .0000022000 !.0000027020 
 !     LAW 44
-       SOL1TNL(44,1) = .0000001967 !.0000003088 
-       SOL8TNL(44,1) = .0000011650 !.0000016320 
-       TET4TNL(44,1) = .0000002167 !.0000002999 
-       TET10TNL(44,1)= .0000008448 !.0000010160 
+       SOL1TNL(44,1) = .0000003162
+       SOL8TNL(44,1) = .0000025400
+       TET4TNL(44,1) = .0000003162
+       TET10TNL(44,1)= .0000012600
 !     LAW 48
        SOL1TNL(48,1) = .0000002001 !.0000002994 
        SOL8TNL(48,1) = .0000011410 !.0000015830 
@@ -370,17 +385,41 @@ C M = 2
        TET4TNL(84,1) = .0000001955 !.0000002489
        TET10TNL(84,1)= .0000007306 !.0000007861
 !     LAW 88
-       SOL1TNL(88,1) = .0000003080 !.0000003902
-       SOL8TNL(88,1) = .0000021500 !.0000296600
-       TET4TNL(88,1) = .0000002950 !.0000003749
-       TET10TNL(88,1)= .0000011800 !.0000014590
+       SOL1TNL(88,1) = .0000051800
+       SOL8TNL(88,1) = .0000421000
+       TET4TNL(88,1) = .0000051800
+       TET10TNL(88,1)= .0000210000
 !     LAW 90
-!       SOL1TNL(90,1) = .000000    
+       SOL1TNL(90,1) = .0000007640
+       SOL8TNL(90,1) = .0000053480
+       TET4TNL(90,1) = .0000007640
+       TET10TNL(90,1)= .0000029032
+!     LAW 90 + ISMSTR=10 or 12
+       SOL1TNL(90,2) = .0000009110
+       SOL8TNL(90,2) = .0000065000
+       TET4TNL(90,2) = .0000009110
+       TET10TNL(90,2)= .0000034900
 !     LAW 92
-       SOL1TNL(92,1) = .0000003761 !.0000004932 
-       SOL8TNL(92,1) = .0000026960 !.0000037140 
-       TET4TNL(92,1) = .0000003541 !.0000004553 
-       TET10TNL(92,1)= .0000013580 !.0000017860 
+       SOL1TNL(92,1) = .0000006540
+       SOL8TNL(92,1) = .0000052800
+       TET4TNL(92,1) = .0000006540
+       TET10TNL(92,1)= .0000026100
+!     LAW 92 + ISMSTR=10 or 12
+       SOL1TNL(92,2) = .0000006900
+       SOL8TNL(92,2) = .0000054000  
+       TET4TNL(92,2) = .0000006900  
+       TET10TNL(92,2)= .0000028500 
+!     LAW 94
+       SOL1TNL(94,1) = .0000003650
+       SOL8TNL(94,1) = .0000029200
+       TET4TNL(94,1) = .0000003650
+       TET10TNL(94,1)= .0000014600
+!     LAW 94 + ISMSTR=10 or 12
+       SOL1TNL(94,2) = .0000003900
+       SOL8TNL(94,2) = .0000033900
+       TET4TNL(94,2) = .0000003900
+       TET10TNL(94,2)= .0000016900
+
 !     LAW 95
        SOL1TNL(95,1) = .0000004170 !.0000005950 
        SOL8TNL(95,1) = .0000031200 !.0000045150 
@@ -410,10 +449,10 @@ C M = 2
        TET4TNL(100,3) = .0000001550 !.0000002000
        TET10TNL(100,3)= .0000001550 !.0000002000 
 !     LAW 101
-!       SOL1TNL(101,1) = .0000002493
-!       SOL8TNL(101,1) = .0000018090
-!       TET4TNL(101,1) = .0000002246
-!       TET10TNL(101,1)= .0000009194
+       SOL1TNL(101,1) = .0000058500
+       SOL8TNL(101,1) = .0000468000
+       TET4TNL(101,1) = .0000058500
+       TET10TNL(101,1)= .0000234900
 !     LAW 102
        SOL1TNL(102,1) = .0000002074
        SOL8TNL(102,1) = .0000014790
@@ -476,6 +515,9 @@ C M = 2
 !        2 --> TETRA 4 with IROT = 1
 !        TETRA10ELT
 !        1 --> TETRA10
+!
+!        ELM_OVER_COST :
+!        1 --> ismstr=10 or 12
 
          SOLTELT(1)  =.0000005799 !.0000009528 
          SOLTELT(2)  =.0000005446 !.0000008540 
@@ -494,6 +536,8 @@ C M = 2
          TET4TELT(2) =.0000013046 !.0000016460 
          TET10TELT(1)=.0000014747 !.0000018340 
          NLOCAL_OPTION(1) = .0000001500
+
+         ELM_OVER_COST(1) = .0000000780
        ELSE
          SOLTELT(1)  =.0000004832
          SOLTELT(2)  =.0000004538
@@ -511,6 +555,8 @@ C M = 2
          TET4TELT(2) =.0000010870
          TET10TELT(1)=.0000012200 
          NLOCAL_OPTION(1) = .0000001500
+
+         ELM_OVER_COST(1) = .0000000650
        ENDIF
 
 ! -------------

common_source/includes/weights_p4linux964_spmd_avx512.inc
@@ -71,10 +71,15 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
 !       special option for non-local
         NLOCAL_OPTION(1:10) = 0.
 !     LAW 01
-       SOL1TNL(1,1) = .0000000858  !.0000000556  !.0000001301    
-       SOL8TNL(1,1) = .0000001996  !.0000001336  !.0000003212    
-       TET4TNL(1,1) = .0000000762  !.0000000495  !.0000001229   
-       TET10TNL(1,1)= .0000002815  !.0000001925  !.0000004662    
+       SOL1TNL(1,1) = .0000000858
+       SOL8TNL(1,1) = .0000001996
+       TET4TNL(1,1) = .0000000762
+       TET10TNL(1,1)= .0000002815   
+!     LAW 01 + ISMSTR = 10 or 12
+       SOL1TNL(1,2) = .0000001487
+       SOL8TNL(1,2) = .0000012190
+       TET4TNL(1,2) = .0000001487
+       TET10TNL(1,2)= .0000006840   
 !     LAW 02
        SOL1TNL(2,1) = .0000001047  !.0000000700  !.0000001512    
        SOL8TNL(2,1) = .0000004822  !.0000003250  !.0000006297   
@@ -131,10 +136,15 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
        TET4TNL(24,1) = .0000002885  !.0000001965  !.0000004260
        TET10TNL(24,1)= .0000011390  !.0000008094  !.0000020050
 !     LAW 25
-       SOL1TNL(25,1) = .0000002168  !.0000001463  !.0000004227
-       SOL8TNL(25,1) = .0000016240  !.0000011740  !.0000031570 
-       TET4TNL(25,1) = .0000002085  !.0000001388  !.0000003445 
-       TET10TNL(25,1)= .0000008234  !.0000005631  !.0000016050 
+       SOL1TNL(25,1) = .0000002549
+       SOL8TNL(25,1) = .0000019950
+       TET4TNL(25,1) = .0000002605
+       TET10TNL(25,1)= .0000010080
+!     LAW 25 Crasurv
+       SOL1TNL(25,2) = .0000003029
+       SOL8TNL(25,2) = .0000024380
+       TET4TNL(25,2) = .0000003029
+       TET10TNL(25,2)= .0000011930
 !     LAW 28
        SOL1TNL(28,1) = .0000002598  !.0000002796  !.0000003664 
        SOL8TNL(28,1) = .0000017400  !.0000019590  !.0000021385 
@@ -176,10 +186,15 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
        TET4TNL(36,3) = .0000002000  !.0000001150  !.0000002854 
        TET10TNL(36,3)= .0000007800  !.0000004200  !.0000010970 
 !     LAW 38
-       SOL1TNL(38,1) = .0000004665  !.0000003164  !.0000008197   
-       SOL8TNL(38,1) = .0000033660  !.0000022720  !.0000057300   
-       TET4TNL(38,1) = .0000003855  !.0000002603  !.0000006935   
-       TET10TNL(38,1)= .0000015320  !.0000010100  !.0000025160   
+       SOL1TNL(38,1) = .0000005560
+       SOL8TNL(38,1) = .0000041000
+       TET4TNL(38,1) = .0000005560
+       TET10TNL(38,1)= .0000022240
+!     LAW 38 + ISMSTR=10 or 12
+       SOL1TNL(38,2) = .0000006012
+       SOL8TNL(38,2) = .0000047800
+       TET4TNL(38,2) = .0000006012
+       TET10TNL(38,2)= .0000023900
 !     LAW 40
        SOL1TNL(40,1) = .0000002040  !.0000001303  !.0000004917   
        SOL8TNL(40,1) = .0000013660  !.0000008978  !.0000030250   
@@ -206,10 +221,10 @@ C M = 2
        TET4TNL(42,3) = .0000002901  !.0000001899  !.0000006823 
        TET10TNL(42,3)= .0000011330  !.0000007831  !.0000027020 
 !     LAW 44
-       SOL1TNL(44,1) = .0000001750  !.0000001120  !.0000003088 
-       SOL8TNL(44,1) = .0000010900  !.0000007326  !.0000016320 
-       TET4TNL(44,1) = .0000001775  !.0000001194  !.0000002999 
-       TET10TNL(44,1)= .0000006547  !.0000004164  !.0000010160 
+       SOL1TNL(44,1) = .0000002750
+       SOL8TNL(44,1) = .0000022000
+       TET4TNL(44,1) = .0000002775
+       TET10TNL(44,1)= .0000011000
 !     LAW 48
        SOL1TNL(48,1) = .0000001872  !.0000001223  !.0000002994 
        SOL8TNL(48,1) = .0000012240  !.0000008252  !.0000015830 
@@ -304,10 +319,10 @@ C M = 2
        TET4TNL(70,3) = .0000000005  
        TET10TNL(70,3)= .0000000020 
 !     LAW 71
-       SOL1TNL(71,1) = .0000003106  !.0000002139  !.0000006332 
-       SOL8TNL(71,1) = .0000022870  !.0000005987  !.0000021590 
-       TET4TNL(71,1) = .0000001963  !.0000001303  !.0000004840 
-       TET10TNL(71,1)= .0000007662  !.0000004969  !.0000018860 
+       SOL1TNL(71,1) = .0000003106
+       SOL8TNL(71,1) = .0000022870
+       TET4TNL(71,1) = .0000001963
+       TET10TNL(71,1)= .0000007662
 !     LAW 72
        SOL1TNL(72,1) = .0000001604  !.0000001024  !.0000004335 
        SOL8TNL(72,1) = .0000011490  !.0000007460  !.0000032810 
@@ -370,17 +385,40 @@ C M = 2
        TET4TNL(84,1) = .0000001490  !.0000001001  !.0000002489
        TET10TNL(84,1)= .0000005753  !.0000003790  !.0000007861
 !     LAW 88
-       SOL1TNL(88,1) = .0000001900  !.0000003902
-       SOL8TNL(88,1) = .0000014000  !.0000296600
-       TET4TNL(88,1) = .0000001800  !.0000003749
-       TET10TNL(88,1)= .0000006300  !.0000014590
+       SOL1TNL(88,1) = .0000039930
+       SOL8TNL(88,1) = .0000301000
+       TET4TNL(88,1) = .0000039930
+       TET10TNL(88,1)= .0000156000
 !     LAW 90
-!       SOL1TNL(90,1) = .000000    
+       SOL1TNL(90,1) = .0000005812 
+       SOL8TNL(90,1) = .0000040500
+       TET4TNL(90,1) = .0000005812
+       TET10TNL(90,1)= .0000022080
+!     LAW 90 + ISMSTR=10 or 12
+       SOL1TNL(90,2) = .0000007010
+       SOL8TNL(90,2) = .0000051090
+       TET4TNL(90,2) = .0000007010
+       TET10TNL(90,2)= .0000026700
 !     LAW 92
-       SOL1TNL(92,1) = .0000002360  
-       SOL8TNL(92,1) = .0000017340  
-       TET4TNL(92,1) = .0000002193  
-       TET10TNL(92,1)= .0000008390  
+       SOL1TNL(92,1) = .0000005000 
+       SOL8TNL(92,1) = .0000038000  
+       TET4TNL(92,1) = .0000005000  
+       TET10TNL(92,1)= .0000019000
+!     LAW 92 + ISMSTR=10 or 12
+       SOL1TNL(92,2) = .0000005300
+       SOL8TNL(92,2) = .0000041000  
+       TET4TNL(92,2) = .0000005300  
+       TET10TNL(92,2)= .0000021000 
+!     LAW 94
+       SOL1TNL(94,1) = .0000002842
+       SOL8TNL(94,1) = .0000022700
+       TET4TNL(94,1) = .0000002842
+       TET10TNL(94,1)= .0000011040
+!     LAW 94 + ISMSTR=10 or 12
+       SOL1TNL(94,2) = .0000003093
+       SOL8TNL(94,2) = .0000025000
+       TET4TNL(94,2) = .0000003093
+       TET10TNL(94,2)= .0000012300
 !     LAW 95
        SOL1TNL(95,1) = .0000002493  
        SOL8TNL(95,1) = .0000018090  
@@ -411,10 +449,10 @@ C M = 2
        TET4TNL(100,3) = .0000002000
        TET10TNL(100,3)= .0000002000 
 !     LAW 101
-!       SOL1TNL(101,1) = .0000002493
-!       SOL8TNL(101,1) = .0000018090
-!       TET4TNL(101,1) = .0000002246
-!       TET10TNL(101,1)= .0000009194
+       SOL1TNL(101,1) = .0000045120
+       SOL8TNL(101,1) = .0000356000
+       TET4TNL(101,1) = .0000045120
+       TET10TNL(101,1)= .0000179000
 !     LAW 102
        SOL1TNL(102,1) = .0000001649
        SOL8TNL(102,1) = .0000011640
@@ -477,6 +515,9 @@ C M = 2
 !        2 --> TETRA 4 with IROT = 1
 !        TETRA10ELT
 !        1 --> TETRA10
+!
+!        ELM_OVER_COST :
+!        1 --> ismstr=10 or 12
 
          SOLTELT(1)  =.0000003163 
          SOLTELT(2)  =.0000002903 
@@ -495,6 +536,8 @@ C M = 2
          TET4TELT(1) =.0000001979 
          TET4TELT(2) =.0000007503 
          TET10TELT(1)=.0000008108 
+
+         ELM_OVER_COST(1) = .0000000600
        ELSE
          SOLTELT(1)  =.0000002635
          SOLTELT(2)  =.0000002419
@@ -512,6 +555,8 @@ C M = 2
          TET4TELT(1) =.0000001646
          TET4TELT(2) =.0000006252
          TET10TELT(1)=.0000006756
+
+         ELM_OVER_COST(1) = .0000000500
        ENDIF
 
 ! -------------

common_source/includes/weights_p4linux964_spmd_sse3.inc
@@ -76,6 +76,11 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
        SOL8TNL(1,1) = .0000003212    
        TET4TNL(1,1) = .0000001229   
        TET10TNL(1,1)= .0000004662    
+!     LAW 01 + ISMSTR = 10 or 12
+       SOL1TNL(1,2) = .0000002254
+       SOL8TNL(1,2) = .0000018500
+       TET4TNL(1,2) = .0000002254
+       TET10TNL(1,2)= .0000009016
 !     LAW 02
        SOL1TNL(2,1) = .0000001512    
        SOL8TNL(2,1) = .0000006297   
@@ -132,15 +137,25 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
        TET4TNL(24,1) = .0000004260
        TET10TNL(24,1)= .0000020050
 !     LAW 25
-       SOL1TNL(25,1) = .0000004227
-       SOL8TNL(25,1) = .0000031570 
+       SOL1TNL(25,1) = .0000005039
+       SOL8TNL(25,1) = .0000040300
        TET4TNL(25,1) = .0000003445 
-       TET10TNL(25,1)= .0000016050 
+       TET10TNL(25,1)= .0000020100
+!     LAW 25
+       SOL1TNL(25,1) = .0000006250
+       SOL8TNL(25,1) = .0000050290
+       TET4TNL(25,1) = .0000006250
+       TET10TNL(25,1)= .0000025400
+!     LAW 25 Crasurv
+       SOL1TNL(25,2) = .0000007571
+       SOL8TNL(25,2) = .0000060500
+       TET4TNL(25,2) = .0000007571
+       TET10TNL(25,2)= .0000030280
 !     LAW 28
-       SOL1TNL(28,1) = .0000003664 
-       SOL8TNL(28,1) = .0000021385 
-       TET4TNL(28,1) = .0000003168 
-       TET10TNL(28,1)= .0000013620 
+       SOL1TNL(28,1) = .0000007328
+       SOL8TNL(28,1) = .0000058600
+       TET4TNL(28,1) = .0000007328
+       TET10TNL(28,1)= .0000029300
 !     LAW 33  0 fct
        SOL1TNL(33,1) = .0000004453 
        SOL8TNL(33,1) = .0000021870 
@@ -180,7 +195,12 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
        SOL1TNL(38,1) = .0000008197   
        SOL8TNL(38,1) = .0000057300   
        TET4TNL(38,1) = .0000006935   
-       TET10TNL(38,1)= .0000025160   
+       TET10TNL(38,1)= .0000025160 
+!     LAW 38 + ISMSTR=10 or 12
+       SOL1TNL(38,2) = .0000009800
+       SOL8TNL(38,2) = .0000080100
+       TET4TNL(38,2) = .0000009800
+       TET10TNL(38,2)= .0000037800
 !     LAW 40
        SOL1TNL(40,1) = .0000004917   
        SOL8TNL(40,1) = .0000030250   
@@ -210,6 +230,11 @@ C M = 2
        SOL8TNL(44,1) = .0000016320 
        TET4TNL(44,1) = .0000002999 
        TET10TNL(44,1)= .0000010160 
+!     LAW 44
+       SOL1TNL(44,1) = .0000004850
+       SOL8TNL(44,1) = .0000037800
+       TET4TNL(44,1) = .0000004850
+       TET10TNL(44,1)= .0000018700
 !     LAW 48
        SOL1TNL(48,1) = .0000002994 
        SOL8TNL(48,1) = .0000015830 
@@ -370,17 +395,40 @@ C M = 2
        TET4TNL(84,1) = .0000002489
        TET10TNL(84,1)= .0000007861
 !     LAW 88
-       SOL1TNL(88,1) = .0000003902
-       SOL8TNL(88,1) = .0000296600
-       TET4TNL(88,1) = .0000003749
-       TET10TNL(88,1)= .0000014590
+       SOL1TNL(88,1) = .0000080930
+       SOL8TNL(88,1) = .0000600000
+       TET4TNL(88,1) = .0000080930
+       TET10TNL(88,1)= .0000302000
 !     LAW 90
-!       SOL1TNL(90,1) = .000000    
+       SOL1TNL(90,1) = .0000011200
+       SOL8TNL(90,1) = .0000089600
+       TET4TNL(90,1) = .0000011200
+       TET10TNL(90,1)= .0000044800
+!     LAW 90 + ISMSTR=10 or 12
+       SOL1TNL(90,1) = .0000014000
+       SOL8TNL(90,1) = .0000096600
+       TET4TNL(90,1) = .0000014200
+       TET10TNL(90,1)= .0000047000
 !     LAW 92
-       SOL1TNL(92,1) = .0000004932 
-       SOL8TNL(92,1) = .0000037140 
-       TET4TNL(92,1) = .0000004553 
-       TET10TNL(92,1)= .0000017860 
+       SOL1TNL(92,1) = .0000009800 
+       SOL8TNL(92,1) = .0000088000  
+       TET4TNL(92,1) = .0000009800  
+       TET10TNL(92,1)= .0000041400
+!     LAW 92 + ISMSTR=10 or 12
+       SOL1TNL(92,2) = .0000011400
+       SOL8TNL(92,2) = .0000090000  
+       TET4TNL(92,2) = .0000011400
+       TET10TNL(92,2)= .0000044000
+!     LAW 94
+       SOL1TNL(94,1) = .0000006230
+       SOL8TNL(94,1) = .0000051800
+       TET4TNL(94,1) = .0000006230
+       TET10TNL(94,1)= .0000024900
+!     LAW 94 + ISMSTR=10 or 12
+       SOL1TNL(94,2) = .0000007010
+       SOL8TNL(94,2) = .0000056800
+       TET4TNL(94,2) = .0000007010
+       TET10TNL(94,2)= .0000027900
 !     LAW 95
        SOL1TNL(95,1) = .0000005950 
        SOL8TNL(95,1) = .0000045150 
@@ -410,10 +458,10 @@ C M = 2
        TET4TNL(100,3) = .0000002000
        TET10TNL(100,3)= .0000002000 
 !     LAW 101
-!       SOL1TNL(101,1) = .0000002493
-!       SOL8TNL(101,1) = .0000018090
-!       TET4TNL(101,1) = .0000002246
-!       TET10TNL(101,1)= .0000009194
+       SOL1TNL(101,1) = .0000090800
+       SOL8TNL(101,1) = .0000749000
+       TET4TNL(101,1) = .0000090800
+       TET10TNL(101,1)= .0000359000
 !     LAW 102
        SOL1TNL(102,1) = .0000002770
        SOL8TNL(102,1) = .0000020330
@@ -474,6 +522,9 @@ C M = 2
 !        2 --> TETRA 4 with IROT = 1
 !        TETRA10ELT
 !        1 --> TETRA10
+!
+!        ELM_OVER_COST :
+!        1 --> ismstr=10 or 12
 
          SOLTELT(1)  =.0000009528 
          SOLTELT(2)  =.0000008540 
@@ -489,6 +540,8 @@ C M = 2
          TET4TELT(2) =.0000016460 
          TET10TELT(1)=.0000018340 
          NLOCAL_OPTION(1) = .0000002450
+
+         ELM_OVER_COST(1) = .0000001700
        ELSE
          SOLTELT(1)  =.0000006901 
          SOLTELT(2)  =.0000005650 
@@ -504,6 +557,8 @@ C M = 2
          TET4TELT(2) =.0000014960 
          TET10TELT(1)=.0000016200 
          NLOCAL_OPTION(1) = .0000002450
+
+         ELM_OVER_COST(1) = .0000001400
        ENDIF
 
 ! -------------

commit c3fcac5655103ed3ee2fbdf77630684d6e5b8431

rigid wall, fix SPMD regression from bd168ce

starter/source/constraints/general/rwall/split_rwall.F90
@@ -105,6 +105,7 @@
                 call plist_ifront(proc_list,node_id,proc_number) ! get the list of proce where S node is defined
                 do j=1,proc_number
                   proc_id = proc_list(j)
+                  if(constraint_struct%rwall%spmd(n)%m_proc_list(proc_id) == 0) cycle
                   constraint_struct%rwall%dd(proc_id,n) = constraint_struct%rwall%dd(proc_id,n) + 1
                 enddo
               enddo
@@ -116,6 +117,7 @@
             proc_main = 1
             max_node_number = 0
             do i=1,nspmd
+              if(constraint_struct%rwall%spmd(n)%m_proc_list(i) == 0) cycle
               if(constraint_struct%rwall%dd(i,n)>max_node_number) then
                 max_node_number = constraint_struct%rwall%dd(i,n)
                 proc_main = i