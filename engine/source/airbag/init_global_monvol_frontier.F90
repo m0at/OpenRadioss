m0at/OpenRadioss

commit b8b298388691026974630380da1b5ed806f1d333

Airbag optimization : add omp directive and reduce the number of mpi comm

engine/source/airbag/init_global_monvol_frontier.F90
@@ -1,0 +1,137 @@
+!Copyright>        OpenRadioss
+!Copyright>        Copyright (C) 1986-2024 Altair Engineering Inc.
+!Copyright>
+!Copyright>        This program is free software: you can redistribute it and/or modify
+!Copyright>        it under the terms of the GNU Affero General Public License as published by
+!Copyright>        the Free Software Foundation, either version 3 of the License, or
+!Copyright>        (at your option) any later version.
+!Copyright>
+!Copyright>        This program is distributed in the hope that it will be useful,
+!Copyright>        but WITHOUT ANY WARRANTY; without even the implied warranty of
+!Copyright>        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!Copyright>        GNU Affero General Public License for more details.
+!Copyright>
+!Copyright>        You should have received a copy of the GNU Affero General Public License
+!Copyright>        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+!Copyright>
+!Copyright>
+!Copyright>        Commercial Alternative: Altair Radioss Software
+!Copyright>
+!Copyright>        As an alternative to this open-source version, Altair also offers Altair Radioss
+!Copyright>        software under a commercial license.  Contact Altair to discuss further if the
+!Copyright>        commercial version may interest you: https://www.altair.com/radioss/.
+      module init_global_frontier_monvol_mod
+      contains
+! ======================================================================================================================
+!                                                   procedures
+! ======================================================================================================================
+!! \brief This routine computes the frontier between processor for a monitored volume mpi comm
+!! \details loop over the monitored volumes to find a main processor
+!!          --> the main processor has at least 1 monitored volume
+!!          --> the main processor has the lowest number of segment
+        subroutine init_global_frontier_monvol(ispmd,nspmd,nvolu,nsurf,monvol, &
+                       nimv,    & 
+                       fr_mv,frontier_global_mv, t_monvoln,igrsurf )
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   modules
+! ----------------------------------------------------------------------------------------------------------------------
+          use monvol_struct_mod , only : monvol_struct_
+          use groupdef_mod , only : surf_
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+          implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   included files
+! ----------------------------------------------------------------------------------------------------------------------
+#include "my_real.inc"
+#ifdef MPI
+#include "mpif.h"
+#endif
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   arguments
+! ----------------------------------------------------------------------------------------------------------------------
+          integer :: ispmd !< mpi task id
+          integer, intent(in) :: nspmd !< total number of mpi tasks
+          integer, intent(in) :: nvolu !< number of monitored volume
+          integer, intent(in) :: nsurf !< number of surface    
+          integer, intent(in) :: nimv !< first dim of monvol
+          integer, dimension(nspmd+2,nvolu), intent(in) :: fr_mv !< mpi frontier per monitored volume
+          integer, dimension(nspmd+2), intent(inout) :: frontier_global_mv !< global mpi frontier 
+          integer, dimension(nimv*nvolu), intent(in) :: monvol !< monitored volume data
+          type(monvol_struct_), dimension(nvolu), intent(in) :: t_monvoln
+          type(surf_), dimension(nsurf), intent(in) :: igrsurf
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   local variables
+! ----------------------------------------------------------------------------------------------------------------------
+          logical :: computation_needed
+          integer :: segment_number,surf_id,ityp
+          integer :: p_main,min_segment
+          integer :: monvol_address
+          integer :: ijk
+          integer, dimension(2) :: s_buffer
+          integer, dimension(2,nspmd) :: r_buffer
+          integer :: ierror
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   external functions
+! ----------------------------------------------------------------------------------------------------------------------
+! [ external functions must be kept to mimimum ]
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   body
+! ----------------------------------------------------------------------------------------------------------------------
+          s_buffer(1:2) = 0
+          monvol_address = 1
+          do ijk=1,nvolu
+            ityp = monvol(monvol_address+1) ! get the type of the airbag
+            surf_id = monvol(monvol_address+3) ! get the id of the surface
+            segment_number = igrsurf(surf_id)%nseg ! get the number of segment of the surface "surf_id"
+            computation_needed = .true.
+            if(ityp==6.or.ityp==8) computation_needed = .false.
+            if(fr_mv(ispmd+1,ijk)==0.and.fr_mv(nspmd+2,ijk)/=ispmd+1) computation_needed = .false.
+
+            if(computation_needed) then
+              s_buffer(1) = s_buffer(1) + 6
+              s_buffer(2) = s_buffer(2) + segment_number
+              if((t_monvoln(ijk)%nb_fill_tri>0).and.(ispmd + 1 == fr_mv(nspmd+2,ijk))) then
+                s_buffer(2) = s_buffer(2) + t_monvoln(ijk)%nb_fill_tri               
+              endif
+            endif
+            monvol_address = monvol_address + nimv
+          enddo
+ 
+          if(nspmd>1) then
+#ifdef MPI
+            call mpi_gather(s_buffer,2,MPI_INTEGER,r_buffer,2,MPI_INTEGER,0,MPI_COMM_WORLD,ierror)
+#endif
+          else
+            r_buffer(1:2,1) = s_buffer(1:2)
+          endif
+
+          p_main = -1
+          min_segment = HUGE(min_segment)
+          if(ispmd==0) then
+            r_buffer(1:2,1) = s_buffer(1:2)
+            frontier_global_mv(1:nspmd+2) = 0
+            do ijk=1,nspmd
+              frontier_global_mv(ijk) = r_buffer(1,ijk)
+              if(r_buffer(1,ijk)>0.and.r_buffer(2,ijk)<min_segment) then
+                p_main = ijk
+                min_segment = r_buffer(2,ijk)
+              endif
+            enddo
+            if(p_main==-1) then
+        print*," error :",p_main
+              p_main = 1
+            endif
+            frontier_global_mv(nspmd+2) = p_main
+          endif
+          if(nspmd>1) then    
+#ifdef MPI
+            call mpi_bcast(frontier_global_mv,nspmd+2,MPI_INTEGER,0,MPI_COMM_WORLD,ierror)
+#endif  
+          endif
+
+          return
+! ----------------------------------------------------------------------------------------------------------------------
+        end subroutine init_global_frontier_monvol
+      end module init_global_frontier_monvol_mod

engine/source/airbag/init_monvol_omp_structure.F90
@@ -1,0 +1,191 @@
+!Copyright>        OpenRadioss
+!Copyright>        Copyright (C) 1986-2024 Altair Engineering Inc.
+!Copyright>
+!Copyright>        This program is free software: you can redistribute it and/or modify
+!Copyright>        it under the terms of the GNU Affero General Public License as published by
+!Copyright>        the Free Software Foundation, either version 3 of the License, or
+!Copyright>        (at your option) any later version.
+!Copyright>
+!Copyright>        This program is distributed in the hope that it will be useful,
+!Copyright>        but WITHOUT ANY WARRANTY; without even the implied warranty of
+!Copyright>        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!Copyright>        GNU Affero General Public License for more details.
+!Copyright>
+!Copyright>        You should have received a copy of the GNU Affero General Public License
+!Copyright>        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+!Copyright>
+!Copyright>
+!Copyright>        Commercial Alternative: Altair Radioss Software
+!Copyright>
+!Copyright>        As an alternative to this open-source version, Altair also offers Altair Radioss
+!Copyright>        software under a commercial license.  Contact Altair to discuss further if the
+!Copyright>        commercial version may interest you: https://www.altair.com/radioss/.
+      module init_monvol_omp_structure_mod
+      contains
+! ======================================================================================================================
+!                                                   procedures
+! ======================================================================================================================
+!! \brief This routine initializes the monitored volume structure for !$OMP
+!! \details the !$OMP monitored volume structure is similar to the FSKY array
+        subroutine init_monvol_omp_structure(ispmd,nspmd,nvolu,nsurf,monvol,  &
+                                             nimv,numnod,                     & 
+                                             fr_mv,t_monvoln,igrsurf )
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   modules
+! ----------------------------------------------------------------------------------------------------------------------
+          use monvol_struct_mod , only : monvol_struct_
+          use groupdef_mod , only : surf_
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+          implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   included files
+! ----------------------------------------------------------------------------------------------------------------------
+#include "my_real.inc"
+#ifdef MPI
+#include "mpif.h"
+#endif
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   arguments
+! ----------------------------------------------------------------------------------------------------------------------
+          integer, intent(in) :: ispmd !< mpi task id
+          integer, intent(in) :: nspmd !< total number of mpi tasks
+          integer, intent(in) :: nvolu !< number of monitored volume
+          integer, intent(in) :: nsurf !< number of surface    
+          integer, intent(in) :: nimv
+          integer, intent(in) :: numnod !< number of node
+          integer, dimension(nspmd+2,nvolu), intent(in) :: fr_mv !< mpi frontier per monitored volume
+          integer, dimension(*), intent(in) :: monvol !< monitored volume data
+          type(monvol_struct_), dimension(nvolu), intent(inout) :: t_monvoln !< monvol data
+          type(surf_), dimension(nsurf), intent(in) :: igrsurf !< surface data
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   local variables
+! ----------------------------------------------------------------------------------------------------------------------
+          logical :: computation_needed
+          integer :: segment_number,node_number,surf_id,ityp
+          integer :: i,j
+          integer :: p_main,min_segment
+          integer :: monvol_address
+          integer :: ijk,nod,shift,total_contribution_number
+          integer, dimension(:), allocatable :: w_array,node_id,node_shift
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   external functions
+! ----------------------------------------------------------------------------------------------------------------------
+! [ external functions must be kept to mimimum ]
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   body
+! ----------------------------------------------------------------------------------------------------------------------
+          monvol_address = 1
+          ! --------------
+          ! loop over the monitored volume
+          do ijk=1,nvolu
+            ityp = monvol(monvol_address+1) ! get the type of the airbag
+            surf_id = monvol(monvol_address+3) ! get the id of the surface
+            segment_number = igrsurf(surf_id)%nseg ! get the number of segment of the surface "surf_id"
+            ! check if the computation for this ijk monvol is mandatory
+            computation_needed = .true.
+            if(ityp==6.or.ityp==8) computation_needed = .false.
+            if(ityp==1) computation_needed = .false.
+            if(fr_mv(ispmd+1,ijk)==0.and.fr_mv(nspmd+2,ijk)/=ispmd+1) computation_needed = .false.
+
+            if(computation_needed) then
+              allocate( t_monvoln(ijk)%normal(3,segment_number) )
+              allocate( w_array(numnod) )
+              allocate( node_id(numnod) )
+              w_array(1:numnod) = 0
+              total_contribution_number = 0
+              node_number = 0
+              allocate( t_monvoln(ijk)%omp_output%contribution_index(segment_number,4) )
+              ! ---------------
+              do i=1,segment_number ! loop over the segment of the monvol
+                do j=1,3 ! loop over the nodes
+                  nod = igrsurf(surf_id)%nodes(i,j) ! get the node id
+                  if(w_array(nod)==0) then ! the node is not yet taken into account
+                    node_number = node_number + 1
+                    node_id( node_number ) = nod ! save the node id
+                  endif
+                  w_array(nod) = w_array(nod) + 1 ! add 1 contribution for this node
+                  t_monvoln(ijk)%omp_output%contribution_index(i,j) = w_array(nod)
+                  total_contribution_number = total_contribution_number + 1 ! get the total number of contribution
+                enddo
+                if(igrsurf(surf_id)%eltyp(i)/=7) then ! check if the segment has 4 node
+                  nod = igrsurf(surf_id)%nodes(i,4) ! get the node id
+                  if(w_array(nod)==0) then ! the node is not yet taken into account
+                    node_number = node_number + 1
+                    node_id( node_number ) = nod ! save the node id
+                  endif
+                  w_array(nod) = w_array(nod) + 1 ! add 1 contribution for this node
+                  t_monvoln(ijk)%omp_output%contribution_index(i,4) = w_array(nod)
+                  total_contribution_number = total_contribution_number + 1 ! get the total number of contribution
+                endif                
+              enddo
+              ! ---------------
+
+              ! ---------------
+              ! allocation & initialization
+              allocate( t_monvoln(ijk)%omp_output%contribution(total_contribution_number,3) )
+              allocate( t_monvoln(ijk)%omp_output%contribution_number(node_number+1) )
+              allocate( t_monvoln(ijk)%omp_output%node_id(node_number) )
+              t_monvoln(ijk)%omp_output%node_number = node_number
+              t_monvoln(ijk)%omp_output%total_contribution_number = total_contribution_number
+              t_monvoln(ijk)%omp_output%node_id(1:node_number) = node_id(1:node_number)
+              allocate( node_shift(node_number+1) )
+              node_shift( 1:node_number+1 ) = 0
+              ! ---------------
+
+              ! ---------------
+              ! save the number of contribution + the address
+              shift = 0
+              do i=2,node_number+1
+                nod = node_id(i-1)
+                node_shift( i ) = node_shift( i-1 ) + w_array(nod) ! get the shift for the node "i"
+              enddo
+              ! node :              1  |  2  |  3  |  4  | ...
+              ! #contribution :     4  |  1  |  0  |  4  |
+              ! shift :             0  |  4  |  5  |  5  | 9 ...
+              ! shift(i+1) - shit(i) = #contribution of "i" node
+              ! shift(i) = address for the "i" node    
+              t_monvoln(ijk)%omp_output%contribution_number( 1:node_number+1 ) = node_shift( 1:node_number+1 ) ! save the 
+              ! ---------------
+
+              ! ---------------
+              ! save the index (to point to the good place in CONTRIBUTION array)
+              do i=1,node_number
+                nod = node_id(i)
+                w_array(nod) = node_shift( i )
+              enddo
+              ! loop over the segment
+              do i=1,segment_number
+                do j=1,3
+                  nod = igrsurf(surf_id)%nodes(i,j) ! get the node id
+                  t_monvoln(ijk)%omp_output%contribution_index(i,j) = t_monvoln(ijk)%omp_output%contribution_index(i,j) + &
+                                                                      w_array(nod) ! save the index for the segment "i" and the edge "j)
+                enddo
+                if(igrsurf(surf_id)%eltyp(i)/=7) then ! the segment has 4 node
+                  nod = igrsurf(surf_id)%nodes(i,4) ! get the node id
+                  t_monvoln(ijk)%omp_output%contribution_index(i,4) = t_monvoln(ijk)%omp_output%contribution_index(i,4) + &
+                                                                      w_array(nod) ! save the index for the segment "i" and the edge "j)
+                endif
+              enddo
+              ! ---------------
+              deallocate( node_shift )
+              deallocate( w_array )
+              deallocate( node_id )
+            else
+              allocate( t_monvoln(ijk)%normal(3,0) )
+              t_monvoln(ijk)%omp_output%node_number = 0
+              t_monvoln(ijk)%omp_output%total_contribution_number = 0
+              allocate( t_monvoln(ijk)%omp_output%contribution_number(0) )
+              allocate( t_monvoln(ijk)%omp_output%contribution_index(0,0) )
+              allocate( t_monvoln(ijk)%omp_output%contribution(0,0) )
+              allocate( t_monvoln(ijk)%omp_output%node_id(0) )
+            endif
+            monvol_address = monvol_address + nimv
+          enddo
+          ! --------------
+
+          return
+! ----------------------------------------------------------------------------------------------------------------------
+        end subroutine init_monvol_omp_structure
+      end module init_monvol_omp_structure_mod

engine/share/modules/monvol_struct_mod.F
@@ -39,11 +39,27 @@ C-----------------------------------------------
 C-----------------------------------------------
 C     C o m m o n   B l o c k s
 C-----------------------------------------------
+      ! ---------
+      ! type for OMP accumulation
+      TYPE MONVOL_OMP_
+        INTEGER :: NODE_NUMBER !< number of /= node
+        INTEGER :: TOTAL_CONTRIBUTION_NUMBER !< total number of contribution (if only shells = 4 * NODE_NUMBER)
+        INTEGER, DIMENSION(:), ALLOCATABLE  :: NODE_ID !< node id
+
+        INTEGER, DIMENSION(:), ALLOCATABLE  :: CONTRIBUTION_NUMBER !< number of contribution per node
+        INTEGER, DIMENSION(:,:), ALLOCATABLE  :: CONTRIBUTION_INDEX !< index to the array CONTRIBUTION
+        my_real, DIMENSION(:,:), ALLOCATABLE  :: CONTRIBUTION !< contribution array, the force are saved here
+      END TYPE MONVOL_OMP_
+      ! ---------
+
       TYPE MONVOL_STRUCT_
 !     Automatic surface hole filling
       INTEGER :: NB_FILL_TRI
       INTEGER, DIMENSION(:), ALLOCATABLE :: FILL_TRI
+      my_real, DIMENSION(:,:), ALLOCATABLE  :: NORMAL !< normal
+      TYPE(MONVOL_OMP_) :: OMP_OUTPUT
       END TYPE MONVOL_STRUCT_
+
       
       CONTAINS
       !||====================================================================

engine/source/airbag/airbag2.F
@@ -34,7 +34,7 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
       SUBROUTINE AIRBAGB(
      1                 IVOLU   ,ICBAG   ,NJET    ,IBAGJET ,NVENT   ,
      2                 IBAGHOL ,RVOLU   ,RVOLUV  ,RCBAG   ,RBAGJET ,
-     3                 RBAGHOL ,FSAV    ,N       ,NN      ,
+     3                 RBAGHOL ,FSAV    ,NORMAL  ,NN      ,
      4                 IGRSURF ,PORO    ,IVOLUV  ,RBAGVJET,
      4                 FR_MV   )
 C-----------------------------------------------
@@ -66,7 +66,7 @@ C-----------------------------------------------
 C     REAL
       my_real
      .   RVOLU(*), RVOLUV(NRVOLU,*),RCBAG(NRCBAG,*),PORO(*),
-     .   RBAGJET(NRBJET,*),RBAGHOL(NRBHOL,*),FSAV(*),N(3,*),RBAGVJET(*)
+     .   RBAGJET(NRBJET,*),RBAGHOL(NRBHOL,*),FSAV(*),NORMAL(3,*),RBAGVJET(*)
       TYPE (SURF_)   , DIMENSION(NSURF)   :: IGRSURF
 C-----------------------------------------------
 C   L o c a l   V a r i a b l e s
@@ -248,7 +248,7 @@ C
                ELSE
                  K=IGRSURF(IPVENT)%ELEM(KK) + NUMELC + NUMELTG
                ENDIF
-               F1(KK) = SQRT( N(1,K)**2+N(2,K)**2+N(3,K)**2 )
+               F1(KK) = SQRT( NORMAL(1,K)**2+NORMAL(2,K)**2+NORMAL(3,K)**2 )
                F2(KK) = ZERO
              ENDDO
            ELSE
@@ -261,7 +261,7 @@ C
                ELSE
                  K=IGRSURF(IPVENT)%ELEM(KK) + NUMELC + NUMELTG
                ENDIF
-               AA   = SQRT( N(1,K)**2+N(2,K)**2+N(3,K)**2 )
+               AA   = SQRT( NORMAL(1,K)**2+NORMAL(2,K)**2+NORMAL(3,K)**2 )
                F1(KK) = (ONE - PORO(K))*AA
                F2(KK) =  PORO(K)*AA
              ENDDO
@@ -487,7 +487,7 @@ C
                ELSE
                  K=IGRSURF(IPVENT)%ELEM(KK) + NUMELC + NUMELTG
                ENDIF
-               F1(KK) = SQRT( N(1,K)**2+N(2,K)**2+N(3,K)**2 )
+               F1(KK) = SQRT( NORMAL(1,K)**2+NORMAL(2,K)**2+NORMAL(3,K)**2 )
              ENDDO
 C
 C Sommation p/on

engine/source/airbag/airbagb1.F
@@ -38,7 +38,7 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
       SUBROUTINE AIRBAGB1(
      1                 IVOLU   ,ICBAG   ,NJET     ,IBAGJET ,NVENT   , 
      2                 IBAGHOL ,RVOLU   ,RVOLUV   ,RCBAG   ,RBAGJET , 
-     3                 RBAGHOL ,FSAV    ,N        ,NN      , 
+     3                 RBAGHOL ,FSAV    ,NORMAL   ,NN      , 
      4                 IGRSURF ,PORO    ,IVOLUV   ,RBAGVJET,
      5                 FR_MV   ,IPARG   ,IPART    ,IPARTC  ,IPARTTG , 
      6                 IPM     ,PM      ,ELBUF_TAB,IGROUPC ,IGROUPTG,
@@ -78,7 +78,7 @@ C-----------------------------------------------
 C     REAL
       my_real
      .   RVOLU(*), RVOLUV(NRVOLU,*),RCBAG(NRCBAG,*),PORO(*),
-     .   RBAGJET(NRBJET,*),RBAGHOL(NRBHOL,*),FSAV(*),N(3,*),RBAGVJET(*),
+     .   RBAGJET(NRBJET,*),RBAGHOL(NRBHOL,*),FSAV(*),NORMAL(3,*),RBAGVJET(*),
      .   GEO(NPROPG,*), PM(NPROPM,*)
 C
       TYPE(ELBUF_STRUCT_), DIMENSION(NGROUP) :: ELBUF_TAB
@@ -332,7 +332,7 @@ C-------------
                ELSE
                  K=IGRSURF(IPVENT)%ELEM(KK) + NUMELC + NUMELTG
                ENDIF
-               AA = SQRT(N(1,K)**2+N(2,K)**2+N(3,K)**2)
+               AA = SQRT(NORMAL(1,K)**2+NORMAL(2,K)**2+NORMAL(3,K)**2)
                IF(INTBAG==0) THEN
                  F1(KK) = AA
                  F2(KK) = ZERO
@@ -421,7 +421,7 @@ C
 
  200           CONTINUE
                IF (ITY==7) K=K+NUMELC
-               AA   = SQRT( N(1,K)**2+N(2,K)**2+N(3,K)**2 )
+               AA   = SQRT( NORMAL(1,K)**2+NORMAL(2,K)**2+NORMAL(3,K)**2 )
                IF(INTBAG==0) THEN
                  F1(KK) = AA*SVTFAC
                  F2(KK) = ZERO
@@ -784,7 +784,7 @@ C
                ELSE
                  K=IGRSURF(IPVENT)%ELEM(KK) + NUMELC + NUMELTG
                ENDIF
-               F1(KK) = SQRT( N(1,K)**2+N(2,K)**2+N(3,K)**2 )
+               F1(KK) = SQRT( NORMAL(1,K)**2+NORMAL(2,K)**2+NORMAL(3,K)**2 )
              ENDDO
 C
 C Sommation p/on

commit 605241cbce2dd1e9e82795dc4d93ed1e4bd1b569

Fix an issue introduced by b8b298388691026974630380da1b5ed806f1d333 : a type 8 airbag can be transformed into type 7 when a condition is reached, some arrays allocated at the beginning of the engine only for airbag type 1,2,3,4,5,7,9, >10 must also be allocated for type 8

engine/source/airbag/init_global_monvol_frontier.F90
@@ -100,7 +100,7 @@
             surf_id = monvol(monvol_address+3) ! get the id of the surface
             segment_number = igrsurf(surf_id)%nseg ! get the number of segment of the surface "surf_id"
             computation_needed = .true.
-            if(ityp==6.or.ityp==8) computation_needed = .false.
+            if(ityp==6) computation_needed = .false.
             if(fr_mv(ispmd+1,ijk)==0.and.fr_mv(nspmd+2,ijk)/=ispmd+1) computation_needed = .false.
 
             if(computation_needed) then

engine/source/airbag/init_monvol_omp_structure.F90
@@ -97,7 +97,7 @@
             segment_number = igrsurf(surf_id)%nseg ! get the number of segment of the surface "surf_id"
             ! check if the computation for this ijk monvol is mandatory
             computation_needed = .true.
-            if(ityp==6.or.ityp==8) computation_needed = .false.
+            if(ityp==6) computation_needed = .false.
             if(fr_mv(ispmd+1,ijk)==0.and.fr_mv(nspmd+2,ijk)/=ispmd+1) computation_needed = .false.
 
             if(computation_needed) then