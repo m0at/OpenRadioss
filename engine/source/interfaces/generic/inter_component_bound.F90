m0at/OpenRadioss

commit e667c537cf05c15f225042b2654023c6282c8383

/TYPE7 performance improvement : add the concept of component to reduce the amount of mpi communication in the collision detection algorithm

engine/source/interfaces/generic/inter_component_bound.F90
@@ -1,0 +1,105 @@
+!Copyright>        OpenRadioss
+!Copyright>        Copyright (C) 1986-2025 Altair Engineering Inc.
+!Copyright>
+!Copyright>        This program is free software: you can redistribute it and/or modify
+!Copyright>        it under the terms of the GNU Affero General Public License as published by
+!Copyright>        the Free Software Foundation, either version 3 of the License, or
+!Copyright>        (at your option) any later version.
+!Copyright>
+!Copyright>        This program is distributed in the hope that it will be useful,
+!Copyright>        but WITHOUT ANY WARRANTY; without even the implied warranty of
+!Copyright>        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!Copyright>        GNU Affero General Public License for more details.
+!Copyright>
+!Copyright>        You should have received a copy of the GNU Affero General Public License
+!Copyright>        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+!Copyright>
+!Copyright>
+!Copyright>        Commercial Alternative: Altair Radioss Software
+!Copyright>
+!Copyright>        As an alternative to this open-source version, Altair also offers Altair Radioss
+!Copyright>        software under a commercial license.  Contact Altair to discuss further if the
+!Copyright>        commercial version may interest you: https://www.altair.com/radioss/.
+      module inter_component_bound_mod
+      contains
+! ======================================================================================================================
+!                                                   procedures
+! ======================================================================================================================
+!! \brief Here is a small description of the routine, [after the header]
+!! \details if needed, more details can be added here
+        subroutine inter_component_bound(numnod,tzinf,curv,x,component)
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Modules
+! ----------------------------------------------------------------------------------------------------------------------
+        use constant_mod
+        use inter_sorting_mod , only : component_
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+          implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Included files
+! ----------------------------------------------------------------------------------------------------------------------
+#include "my_real.inc"
+#include "spmd.inc"
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Arguments
+! ----------------------------------------------------------------------------------------------------------------------
+        integer, intent(in) :: numnod !< number of nodes
+        my_real, intent(in) :: tzinf !< max tzinf
+        my_real, intent(in) :: curv !< max curv
+        my_real, dimension(3,numnod), intent(in) :: x !< node coordinates
+        type(component_), intent(inout) :: component
+
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Local variables
+! ----------------------------------------------------------------------------------------------------------------------
+        integer :: i,ncomp
+        integer :: node_id
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   External functions
+! ----------------------------------------------------------------------------------------------------------------------
+
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Body
+! ----------------------------------------------------------------------------------------------------------------------
+        do ncomp=1,component%s_comp_nb
+          component%s_list(ncomp)%bound(1:3) = -ep30
+          component%s_list(ncomp)%bound(4:6) = ep30
+          do i=1,component%s_list(ncomp)%node_nb
+            node_id = component%s_list(ncomp)%node(i)
+            component%s_list(ncomp)%bound(1) = max(component%s_list(ncomp)%bound(1),x(1,node_id))
+            component%s_list(ncomp)%bound(2) = max(component%s_list(ncomp)%bound(2),x(2,node_id))
+            component%s_list(ncomp)%bound(3) = max(component%s_list(ncomp)%bound(3),x(3,node_id))
+            component%s_list(ncomp)%bound(4) = min(component%s_list(ncomp)%bound(4),x(1,node_id))
+            component%s_list(ncomp)%bound(5) = min(component%s_list(ncomp)%bound(5),x(2,node_id))
+            component%s_list(ncomp)%bound(6) = min(component%s_list(ncomp)%bound(6),x(3,node_id))            
+          enddo
+        enddo
+
+        do ncomp=1,component%m_comp_nb
+          component%m_list(ncomp)%bound(1:3) = -ep30
+          component%m_list(ncomp)%bound(4:6) = ep30
+          do i=1,component%m_list(ncomp)%node_nb
+            node_id = component%m_list(ncomp)%node(i)
+            component%m_list(ncomp)%bound(1) = max(component%m_list(ncomp)%bound(1),x(1,node_id))
+            component%m_list(ncomp)%bound(2) = max(component%m_list(ncomp)%bound(2),x(2,node_id))
+            component%m_list(ncomp)%bound(3) = max(component%m_list(ncomp)%bound(3),x(3,node_id))
+            component%m_list(ncomp)%bound(4) = min(component%m_list(ncomp)%bound(4),x(1,node_id))
+            component%m_list(ncomp)%bound(5) = min(component%m_list(ncomp)%bound(5),x(2,node_id))
+            component%m_list(ncomp)%bound(6) = min(component%m_list(ncomp)%bound(6),x(3,node_id))            
+          enddo
+
+          component%m_list(ncomp)%bound(1) = component%m_list(ncomp)%bound(1) + tzinf + curv
+          component%m_list(ncomp)%bound(2) = component%m_list(ncomp)%bound(2) + tzinf + curv
+          component%m_list(ncomp)%bound(3) = component%m_list(ncomp)%bound(3) + tzinf + curv
+          component%m_list(ncomp)%bound(4) = component%m_list(ncomp)%bound(4) - tzinf - curv
+          component%m_list(ncomp)%bound(5) = component%m_list(ncomp)%bound(5) - tzinf - curv
+          component%m_list(ncomp)%bound(6) = component%m_list(ncomp)%bound(6) - tzinf - curv
+        enddo        
+
+
+
+! ----------------------------------------------------------------------------------------------------------------------
+        end subroutine inter_component_bound
+      end module inter_component_bound_mod

engine/share/modules/inter_sorting_mod.F
@@ -65,7 +65,7 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
             INTEGER :: REQUEST_INACTI   ! request for inacti communication
 
             my_real, DIMENSION(6) :: BOX_LIMIT  ! min & max positions of the grid
-            INTEGER, PARAMETER :: NB_BOX_COARSE_GRID=4
+            INTEGER, PARAMETER :: NB_BOX_COARSE_GRID=8
             INTEGER, PARAMETER :: NB_BOX_LIMIT=128
             INTEGER :: NB_CELL_X,NB_CELL_Y,NB_CELL_Z    !   number of cell of the global voxel
 
@@ -116,6 +116,11 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
                 INTEGER, DIMENSION(:), ALLOCATABLE :: SEND_DISPLS_NB_CELL  ! displacement for send
                 INTEGER, DIMENSION(:), ALLOCATABLE :: RCV_DISPLS_NB_CELL   ! displacement for recv
 
+                integer :: request_s_nb
+                integer :: request_r_nb
+                integer, dimension(:), allocatable :: request_s
+                integer, dimension(:), allocatable :: request_r,index_r
+
                 !   -----------------------------
                 !   exchange of cell (fine grid) : mode = 2
                 INTEGER :: REQUEST_NB_CELL                              ! request for cell size exchange
@@ -159,6 +164,45 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
                 INTEGER, DIMENSION(:), ALLOCATABLE :: REQ_SD3       ! request for Send : IREM comm
                 !   -----------------------------
             END TYPE sorting_comm_type
+
+
+            type s_list_
+              integer :: node_nb
+              integer, dimension(:), allocatable :: node
+              my_real, dimension(:), allocatable :: bound
+            end type s_list_
+            type m_list_
+              integer :: node_nb
+              integer, dimension(:), allocatable :: node
+              integer, dimension(:), allocatable :: seg 
+              my_real, dimension(:), allocatable :: bound
+            end type m_list_
+
+            type proc_comp_
+              logical :: need_comm0
+              logical :: need_comm_s
+              logical :: need_comm_r
+              integer :: remote_m_comp_nb
+              my_real, dimension(:,:), allocatable :: remote_m_comp
+              integer :: remote_s_comp_nb
+              my_real, dimension(:,:), allocatable :: remote_s_comp
+              my_real, dimension(:), allocatable :: remote_comp
+            end type proc_comp_
+            type component_
+              integer :: s_comp_nb
+              integer :: m_comp_nb
+              integer :: total_m_remote_comp_nb
+              integer :: total_s_remote_comp_nb
+              integer :: request_s_nb
+              integer :: request_r_nb
+              integer, dimension(:), allocatable :: request_s
+              integer, dimension(:), allocatable :: request_r
+              integer, dimension(:), allocatable :: index_r
+              my_real, dimension(:), allocatable :: s_buffer_comp
+              type(s_list_), dimension(:), allocatable :: s_list
+              type(m_list_), dimension(:), allocatable :: m_list
+              type(proc_comp_), dimension(:), allocatable :: proc_comp
+            end type component_
               
 !   -------------------------    
         END MODULE INTER_SORTING_MOD

engine/source/engine/resol.F
@@ -713,6 +713,7 @@ C-----------------------------------------------
               USE ams_work_mod
               USE UPDATE_PON_MOD
               USE DEBUG_MOD
+              use inter_init_component_mod , only : inter_init_component
 C-----------------------------------------------
 C   I m p l i c i t   T y p e s
 C-----------------------------------------------
@@ -1306,6 +1307,8 @@ C-------------------------------------------------------------------------------
 C-----------------------------------------------------------------------------------
               type(SPH_WORK_)  :: SPH_WORK   !< working Areas for SPLISSV and SPHPREP
               type (ams_work_) :: ams_work   !< Working areas for AMS
+
+              type(component_), dimension(:), allocatable :: component
 C-----------------------------------------------------------------------------------
               ELEMENT%SHELL%OFFSET = NUMELS + NUMELQ
               CALL PREPARE_DEBUG(NODES%ITAB,NUMNOD)
@@ -2391,6 +2394,11 @@ C ---------------------
      .                               numelrg,numeltgg ,IXS(L1))
               ! -------------------------
 
+              ! -------------------------
+              allocate(component(ninter))
+              call inter_init_component(ninter,npari,numnod,ispmd,nspmd,ipari,nodes%x,interfaces%intbuf_tab,component)
+              ! -------------------------
+              
               ! -------------------------
               ! initialization of SPH_WORK
               call ALLOCATE_SPH_WORK(SPH_WORK,
@@ -3746,7 +3754,7 @@ C===============================================================================
      M             LSKYI_SMS_NEW    ,FORNEQS  ,INT7ITIED,IDEL7NOK_SAV,MAXDGAP,
      N             T2FAC_SMS,NODES%ICODT,NODES%ISKEW ,FSKYN25  ,ADDCSRECT,PROCNOR,
      O             INTER_STRUCT,SORT_COMM,RNUM_SIZ,NATIV_SMS_SIZ,TEMP_SIZ,
-     P             INTERFACES,GLOB_THERM)
+     P             INTERFACES,GLOB_THERM,component)
 C
 #include "lockon.inc"
                 IF(DT2TT<DT2T)THEN

engine/source/interfaces/generic/inter_box_creation.F
@@ -27,7 +27,7 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
       !||--- uses       -----------------------------------------------------
       !||    inter_sorting_mod    ../engine/share/modules/inter_sorting_mod.F
       !||====================================================================
-        SUBROUTINE INTER_BOX_CREATION( )
+        SUBROUTINE INTER_BOX_CREATION(nb_cell_x,nb_cell_y,nb_cell_z,box_limit)
 !$COMMENT
 !       INTER_BOX_CREATION description :
 !       creation of the global box
@@ -41,7 +41,7 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
 C-----------------------------------------------
 C   M o d u l e s
 C-----------------------------------------------
-        USE INTER_SORTING_MOD
+        USE INTER_SORTING_MOD , ONLY : NB_BOX_COARSE_GRID,NB_BOX_LIMIT
 C-----------------------------------------------
 C   I m p l i c i t   T y p e s
 C-----------------------------------------------
@@ -54,7 +54,10 @@ C-----------------------------------------------
 C-----------------------------------------------
 C   D u m m y   A r g u m e n t s
 C-----------------------------------------------
-
+        integer, intent(inout) :: nb_cell_x !< number of cell in the x direction
+        integer, intent(inout) :: nb_cell_y !< number of cell in the y direction
+        integer, intent(inout) :: nb_cell_z !< number of cell in the z direction
+        my_real, dimension(6), intent(inout) :: box_limit !< upper & lower bound of the box
 C-----------------------------------------------
 C   L o c a l   V a r i a b l e s
 C-----------------------------------------------

engine/source/interfaces/generic/inter_cell_color.F
@@ -82,7 +82,7 @@ C-----------------------------------------------
      .        XMINE,YMINE,ZMINE,XMAXE,YMAXE,ZMAXE,
      .        XX1,XX2,XX3,XX4,YY1,YY2,YY3,YY4,ZZ1,ZZ2,ZZ3,ZZ4
         INTEGER :: IX_COARSE,IY_COARSE,IZ_COARSE
-        my_real :: VALUE
+        my_real :: VALUE,real_value
 C-----------------------------------------------
 C   S o u r c e  L i n e s
 C-----------------------------------------------
@@ -154,17 +154,19 @@ C-----------------------------------------------
             IY2=MAX(1,MIN(NBY,IY2))
             IZ2=MAX(1,MIN(NBZ,IZ2))
 
+            real_value = float(NB_BOX_COARSE_GRID)
+
             !   loop over the cell and cell coloration
             DO IZ = IZ1, IZ2             
-                VALUE = 4.*float(IZ)/float(NB_CELL_Z)
+                VALUE = real_value*float(IZ)/float(NB_CELL_Z)
                 IZ_COARSE = MIN(INT(VALUE),NB_BOX_COARSE_GRID)
                 IZ_COARSE = MAX(IZ_COARSE,1)
                 DO IY = IY1, IY2
-                    VALUE = 4.*float(IY)/float(NB_CELL_Y)
+                    VALUE = real_value*float(IY)/float(NB_CELL_Y)
                     IY_COARSE = MIN(INT(VALUE),NB_BOX_COARSE_GRID)
                     IY_COARSE = MAX(IY_COARSE,1)
                     DO IX = IX1, IX2
-                        VALUE = 4.*float(IX)/float(NB_CELL_X)
+                        VALUE = real_value*float(IX)/float(NB_CELL_X)
                         IX_COARSE = MIN(INT(VALUE),NB_BOX_COARSE_GRID)
                         IX_COARSE = MAX(IX_COARSE,1)
                         IF(CELL_BOOL(IX,IY,IZ)) THEN

engine/source/interfaces/generic/inter_color_coarse_voxel.F
@@ -83,8 +83,8 @@ C-----------------------------------------------
         ! ------------------
         ! allocation of global omp array 
         IF(ITASK==0) THEN
-            ALLOCATE(CELL_BOOL(4,4,4))
-            CELL_BOOL(1:4,1:4,1:4) = .FALSE.
+            ALLOCATE(CELL_BOOL(NB_BOX_COARSE_GRID,NB_BOX_COARSE_GRID,NB_BOX_COARSE_GRID))
+            CELL_BOOL(1:NB_BOX_COARSE_GRID,1:NB_BOX_COARSE_GRID,1:NB_BOX_COARSE_GRID) = .FALSE.
         ENDIF
         CALL MY_BARRIER()
         ! ------------------
@@ -103,7 +103,7 @@ C-----------------------------------------------
                 XMINB = BOX_LIMIT(4)
                 YMINB = BOX_LIMIT(5)
                 ZMINB = BOX_LIMIT(6)
-                IF(ITASK==0) CELL_BOOL(1:4,1:4,1:4) = .FALSE.
+                IF(ITASK==0) CELL_BOOL(1:NB_BOX_COARSE_GRID,1:NB_BOX_COARSE_GRID,1:NB_BOX_COARSE_GRID) = .FALSE.
                 CALL MY_BARRIER()
                 ! ------------------
                 !   loop over the secondary node to color the coarse grid
@@ -129,9 +129,9 @@ C-----------------------------------------------
                     ENDIF
                     SORT_COMM(NIN)%COARSE_GRID(1:NB_BOX_COARSE_GRID**3 + 1) = 0
                     NEXT = 0
-                    DO IZ=1,4
-                        DO IY=1,4
-                            DO IX=1,4
+                    DO IZ=1,NB_BOX_COARSE_GRID
+                        DO IY=1,NB_BOX_COARSE_GRID
+                            DO IX=1,NB_BOX_COARSE_GRID
                                 IF(CELL_BOOL(IX,IY,IZ)) THEN
                                     NEXT = NEXT + 1
                                     SORT_COMM(NIN)%COARSE_GRID(NEXT) = 

engine/source/interfaces/generic/inter_count_node_curv.F
@@ -89,6 +89,7 @@ C-----------------------------------------------
         my_real :: MX,MY,MZ
         my_real, DIMENSION(3) :: MAX_LIMIT,MIN_LIMIT
         my_real, DIMENSION(3) :: MAX_LIMIT_MAIN,MIN_LIMIT_MAIN
+        my_real, dimension(3) :: max_limit_s,min_limit_s
         my_real, DIMENSION(3) :: SIGMA,SIGMA2
 !   ----------------------------------------
         INTER_STRUCT(NIN)%CURV_MAX_MAX = ZERO
@@ -144,6 +145,22 @@ C-----------------------------------------------
 
         !   ------------------
         !   MIN/MAX secondary node computation 
+        max_limit_s(1:3) = -ep30
+        min_limit_s(1:3) = ep30
+        first = 1+itask*nsn/nthread
+        last = (itask+1)*nsn/nthread
+        do i=first,last
+          nod = intbuf_tab(nin)%nsv(i)
+          if(intbuf_tab(nin)%stfns(i)/=0) then
+            max_limit_s(1) = max(max_limit_s(1),x(1,nod))
+            max_limit_s(2) = max(max_limit_s(2),x(2,nod))
+            max_limit_s(3) = max(max_limit_s(3),x(3,nod))
+
+            min_limit_s(1) = min(min_limit_s(1),x(1,nod))
+            min_limit_s(2) = min(min_limit_s(2),x(2,nod))
+            min_limit_s(3) = min(min_limit_s(3),x(3,nod))            
+          endif
+        enddo
 
         IF(INCONV==1) THEN
             FIRST = 1+ITASK*NSN/NTHREAD
@@ -191,8 +208,16 @@ C-----------------------------------------------
         INTER_STRUCT(NIN)%BOX_LIMIT_MAIN(9) = INTER_STRUCT(NIN)%BOX_LIMIT_MAIN(9)+SIGMA(3)
         INTER_STRUCT(NIN)%BOX_LIMIT_MAIN(10)= INTER_STRUCT(NIN)%BOX_LIMIT_MAIN(10)+SIGMA2(1)
         INTER_STRUCT(NIN)%BOX_LIMIT_MAIN(11)= INTER_STRUCT(NIN)%BOX_LIMIT_MAIN(11)+SIGMA2(2)
-        INTER_STRUCT(NIN)%BOX_LIMIT_MAIN(12)= INTER_STRUCT(NIN)%BOX_LIMIT_MAIN(12)+SIGMA2(3)
+        INTER_STRUCT(NIN)%BOX_LIMIT_MAIN(12)= INTER_STRUCT(NIN)%BOX_LIMIT_MAIN(12)+SIGMA2(3)        
         INTER_STRUCT(NIN)%NMN_G = INTER_STRUCT(NIN)%NMN_G + NMN_L
+
+        box_limit(1) = max(box_limit(1),max_limit_main(1),max_limit_s(1))
+        box_limit(2) = max(box_limit(2),max_limit_main(2),max_limit_s(2))
+        box_limit(3) = max(box_limit(3),max_limit_main(3),max_limit_s(3))
+
+        box_limit(4) = min(box_limit(4),min_limit_main(1),min_limit_s(1))
+        box_limit(5) = min(box_limit(5),min_limit_main(2),min_limit_s(2))
+        box_limit(6) = min(box_limit(6),min_limit_main(3),min_limit_s(3))
 #include "lockoff.inc"
         !   ------------------
         CALL MY_BARRIER

commit 1ddcd5050a991d8d8e00687ffd284fe555609a4f

Fix an omp issue introduced by e667c537cf05c15f225042b2654023c6282c8383

engine/source/interfaces/generic/inter_component_bound.F90
@@ -69,6 +69,7 @@
 ! ----------------------------------------------------------------------------------------------------------------------
         integer :: i,ncomp
         integer :: node_id
+        my_real, dimension(6) :: local_bound
 ! ----------------------------------------------------------------------------------------------------------------------
 !                                                   External functions
 ! ----------------------------------------------------------------------------------------------------------------------
@@ -76,41 +77,78 @@
 ! ----------------------------------------------------------------------------------------------------------------------
 !                                                   Body
 ! ----------------------------------------------------------------------------------------------------------------------
+        
+!$omp single          
         do ncomp=1,component%s_comp_nb
           component%s_list(ncomp)%bound(1:3) = -ep30
           component%s_list(ncomp)%bound(4:6) = ep30
-          do i=1,component%s_list(ncomp)%node_nb
-            node_id = component%s_list(ncomp)%node(i)
-            component%s_list(ncomp)%bound(1) = max(component%s_list(ncomp)%bound(1),x(1,node_id))
-            component%s_list(ncomp)%bound(2) = max(component%s_list(ncomp)%bound(2),x(2,node_id))
-            component%s_list(ncomp)%bound(3) = max(component%s_list(ncomp)%bound(3),x(3,node_id))
-            component%s_list(ncomp)%bound(4) = min(component%s_list(ncomp)%bound(4),x(1,node_id))
-            component%s_list(ncomp)%bound(5) = min(component%s_list(ncomp)%bound(5),x(2,node_id))
-            component%s_list(ncomp)%bound(6) = min(component%s_list(ncomp)%bound(6),x(3,node_id))            
-          enddo
         enddo
-
         do ncomp=1,component%m_comp_nb
           component%m_list(ncomp)%bound(1:3) = -ep30
           component%m_list(ncomp)%bound(4:6) = ep30
+        enddo
+!$omp end single     
+                
+        do ncomp=1,component%s_comp_nb
+          local_bound(1:3) = -ep30
+          local_bound(4:6) = ep30
+!$omp parallel do            
+          do i=1,component%s_list(ncomp)%node_nb
+            node_id = component%s_list(ncomp)%node(i)
+            local_bound(1) = max(local_bound(1),x(1,node_id))
+            local_bound(2) = max(local_bound(2),x(2,node_id))
+            local_bound(3) = max(local_bound(3),x(3,node_id))
+            local_bound(4) = min(local_bound(4),x(1,node_id))
+            local_bound(5) = min(local_bound(5),x(2,node_id))
+            local_bound(6) = min(local_bound(6),x(3,node_id))
+          enddo
+!$omp end parallel do
+
+!$omp critical
+          component%s_list(ncomp)%bound(1) = max(component%s_list(ncomp)%bound(1),local_bound(1))
+          component%s_list(ncomp)%bound(2) = max(component%s_list(ncomp)%bound(2),local_bound(2))
+          component%s_list(ncomp)%bound(3) = max(component%s_list(ncomp)%bound(3),local_bound(3))
+          component%s_list(ncomp)%bound(4) = min(component%s_list(ncomp)%bound(4),local_bound(4))
+          component%s_list(ncomp)%bound(5) = min(component%s_list(ncomp)%bound(5),local_bound(5))
+          component%s_list(ncomp)%bound(6) = min(component%s_list(ncomp)%bound(6),local_bound(6))
+!$omp end critical
+        enddo
+
+        do ncomp=1,component%m_comp_nb
+          local_bound(1:3) = -ep30
+          local_bound(4:6) = ep30
+!$omp parallel do           
           do i=1,component%m_list(ncomp)%node_nb
             node_id = component%m_list(ncomp)%node(i)
-            component%m_list(ncomp)%bound(1) = max(component%m_list(ncomp)%bound(1),x(1,node_id))
-            component%m_list(ncomp)%bound(2) = max(component%m_list(ncomp)%bound(2),x(2,node_id))
-            component%m_list(ncomp)%bound(3) = max(component%m_list(ncomp)%bound(3),x(3,node_id))
-            component%m_list(ncomp)%bound(4) = min(component%m_list(ncomp)%bound(4),x(1,node_id))
-            component%m_list(ncomp)%bound(5) = min(component%m_list(ncomp)%bound(5),x(2,node_id))
-            component%m_list(ncomp)%bound(6) = min(component%m_list(ncomp)%bound(6),x(3,node_id))            
+            local_bound(1) = max(local_bound(1),x(1,node_id))
+            local_bound(2) = max(local_bound(2),x(2,node_id))
+            local_bound(3) = max(local_bound(3),x(3,node_id))
+            local_bound(4) = min(local_bound(4),x(1,node_id))
+            local_bound(5) = min(local_bound(5),x(2,node_id))
+            local_bound(6) = min(local_bound(6),x(3,node_id))
           enddo
+!$omp end parallel do
+          
+!$omp critical          
+            component%m_list(ncomp)%bound(1) = max(component%m_list(ncomp)%bound(1),local_bound(1))
+            component%m_list(ncomp)%bound(2) = max(component%m_list(ncomp)%bound(2),local_bound(2))
+            component%m_list(ncomp)%bound(3) = max(component%m_list(ncomp)%bound(3),local_bound(3))
+            component%m_list(ncomp)%bound(4) = min(component%m_list(ncomp)%bound(4),local_bound(4))
+            component%m_list(ncomp)%bound(5) = min(component%m_list(ncomp)%bound(5),local_bound(5))
+            component%m_list(ncomp)%bound(6) = min(component%m_list(ncomp)%bound(6),local_bound(6))            
+!$omp end critical
+        enddo    
 
+!$omp single          
+        do ncomp=1,component%m_comp_nb
           component%m_list(ncomp)%bound(1) = component%m_list(ncomp)%bound(1) + tzinf + curv
           component%m_list(ncomp)%bound(2) = component%m_list(ncomp)%bound(2) + tzinf + curv
           component%m_list(ncomp)%bound(3) = component%m_list(ncomp)%bound(3) + tzinf + curv
           component%m_list(ncomp)%bound(4) = component%m_list(ncomp)%bound(4) - tzinf - curv
           component%m_list(ncomp)%bound(5) = component%m_list(ncomp)%bound(5) - tzinf - curv
           component%m_list(ncomp)%bound(6) = component%m_list(ncomp)%bound(6) - tzinf - curv
-        enddo        
-
+        enddo
+!$omp end single     
 
 
 ! ----------------------------------------------------------------------------------------------------------------------