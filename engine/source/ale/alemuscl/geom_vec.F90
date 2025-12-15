m0at/OpenRadioss

commit fa99589d7a990a023f2accc468f2003ac2d029c9

ALE : add somes internal timers and first optimizations (gradient reconstruction & array FLUX/QMV)

engine/source/ale/ale51/ale51_init.F
@@ -41,7 +41,8 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
       SUBROUTINE ALE51_INIT(IPARG      ,PM      ,IXS     ,IXQ      ,
      2                      X          ,FLUX    ,FLU2    ,
      3                      ALE_CONNECT,ITASK   ,
-     4                      FLUX_SAV   ,QMV     ,NV46    ,ELBUF_TAB)
+     4                      FLUX_SAV   ,QMV     ,NV46    ,ELBUF_TAB,
+     5                      s_flux,s_qmv)
 C-----------------------------------------------
 C   M o d u l e s
 C-----------------------------------------------
@@ -51,6 +52,12 @@ C-----------------------------------------------
       USE I22TRI_MOD           
       USE ALE_CONNECTIVITY_MOD
       use element_mod , only : nixs
+
+
+
+
+
+      USE DEBUG_MOD
 C-----------------------------------------------
 C   I m p l i c i t   T y p e s
 C-----------------------------------------------
@@ -67,10 +74,12 @@ C-----------------------------------------------
 C-----------------------------------------------
 C   D u m m y   A r g u m e n t s
 C-----------------------------------------------
+       integer, intent(in) :: s_flux !< first dimension of flux array
+      integer, intent(in) :: s_qmv  !< first dimension of QMV array
       TYPE(ELBUF_STRUCT_), TARGET, DIMENSION(NGROUP) :: ELBUF_TAB      
       my_real PM(NPROPM,NUMMAT), X(3,NUMNOD),
-     .        FLUX(NV46,*), FLU2(*),
-     .        FLUX_SAV(NV46,*), QMV(*)
+     .        FLUX(s_flux,nv46), FLU2(*),
+     .        FLUX_SAV(s_flux,nv46), QMV(s_qmv,2*nv46)
       INTEGER IPARG(NPARG,NGROUP), IXS(NIXS,NUMELS), IXQ(7,NUMELQ), ITASK, NV46, J
       TYPE(t_ale_connectivity), INTENT(IN) :: ALE_CONNECT
 C-----------------------------------------------
@@ -108,7 +117,7 @@ C     ALE ON / OFF
         LFT=1                   
         DO K=1,NV46
             DO II=NFT+LFT,NFT+LLT
-              FLUX_SAV(K,II)=FLUX(K,II)
+              FLUX_SAV(ii,k)=FLUX(ii,k)
             ENDDO
         ENDDO        
       END DO !next NG
@@ -139,12 +148,13 @@ C     ALE ON / OFF
         ! UPWIND, QMV, DDVOL           !
         !------------------------------!
         IF(N2D == 0)THEN
-          CALL ALE51_UPWIND3(PM,IXS,FLUX(1,NF1),FLU2(NF1),ALE_CONNECT,
-     +                       0 ,BID,QMV(12*NFT+1),1,
-     +                       NV46 )
+          CALL ALE51_UPWIND3(PM,IXS,FLUX,FLU2(NF1),ALE_CONNECT,
+     +                       0 ,BID,QMV,1,
+     +                       nv46,s_flux,s_qmv)
         ELSE
-          CALL ALE51_UPWIND2(PM,X,IXQ,FLUX(1,NF1),FLU2(NF1),ALE_CONNECT,
-     +                       0,BID,QMV(8*NFT+1),1)
+          CALL ALE51_UPWIND2(PM,X,IXQ,FLUX,FLU2(NF1),ALE_CONNECT,
+     +                       0,BID,QMV,1,
+     +                       nv46,s_flux,s_qmv)
         ENDIF
       END DO !next NG
 

engine/source/ale/alemuscl/ale51_gradient_reconstruction.F
@@ -45,7 +45,7 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
 !||====================================================================
       SUBROUTINE ALE51_GRADIENT_RECONSTRUCTION(IPARG   , ELBUF_TAB, IXS    ,  X       , ALE_CONNECT,
      .                                         NERCVOIS ,NESDVOIS,  LERCVOIS, LESDVOIS   ,LENCOM, ITASK,
-     .                                         IAD_ELEM, FR_ELEM  ,SEGVAR)
+     .                                         IAD_ELEM, FR_ELEM  ,SEGVAR,timers)
 C-----------------------------------------------
 C   D e s c r i p t i o n
 C   Computes limited gradients for volumic fractions
@@ -61,6 +61,18 @@ C-----------------------------------------------
       USE ALE_CONNECTIVITY_MOD
       USE MULTIMAT_PARAM_MOD , ONLY : M51_N0PHAS, M51_NVPHAS
       use element_mod , only :nixs
+      use timer_mod
+      use gradient_reconstruction_mod , only : gradient_reconstruction_new
+      use precision_mod , only : WP
+      use mvsiz_mod, only : MVSIZ
+      use spmd_exch_min_max_mod , only : spmd_exch_min_max
+      use geom_vec_mod , only : geom_vec
+      use spmd_exch_n_neighbor_mod , only : spmd_exch_n_neighbor
+      use array_mod
+
+
+
+      use debug_mod
 C-----------------------------------------------
 C   I m p l i c i t   T y p e s
 C-----------------------------------------------
@@ -82,24 +94,44 @@ C-----------------------------------------------
       INTEGER IPARG(NPARG,*), IXS(NIXS,*)
       my_real :: X(3,*)
       TYPE(ELBUF_STRUCT_), TARGET, DIMENSION(NGROUP) :: ELBUF_TAB
-      INTEGER :: LENCOM, NERCVOIS(*),NESDVOIS(*),LERCVOIS(*),LESDVOIS(*)
-      INTEGER :: IAD_ELEM(2, *), FR_ELEM(*)
+      INTEGER :: LENCOM, NERCVOIS(nspmd+1),NESDVOIS(nspmd+1),LERCVOIS(*),LESDVOIS(*)
+      INTEGER :: IAD_ELEM(2,nspmd+1), FR_ELEM(*)
       TYPE(t_segvar) :: SEGVAR
       TYPE(t_ale_connectivity), INTENT(IN) :: ALE_CONNECT
+      type(timer_), intent(inout) :: timers
 C-----------------------------------------------
 C   L o c a l   V a r i a b l e s
 C-----------------------------------------------
       INTEGER :: NG
       INTEGER :: ITRIMAT
+      integer :: nel,facet_nb
       my_real, DIMENSION(:), POINTER :: VOLG, VOLP, UVAR
       INTEGER :: ADD
       INTEGER :: K, I, II, JJ, NODE_ID, JMIN, JMAX
       INTEGER :: ELEM_ID
-      INTEGER :: FIRST,LAST
-      my_real :: VOL, A(3), B(3), C(3)
+      INTEGER :: FIRST,LAST      
+      my_real :: vol
+      integer :: isolnod,n_entity
+      real(kind=WP), dimension(3,12) :: node_pos
+      real(kind=WP), dimension(mvsiz,3,12) :: a,b,c
+      real(kind=WP), dimension(3) :: a0,b0,c0
+
+      integer :: flag
+      integer :: s_proc_nb_1,r_proc_nb_1,s_proc_nb_2,r_proc_nb_2,s_proc_nb_3,r_proc_nb_3
+      integer :: s_lesdvois,s_lercvois,s_fr_elem,buff_size
+      integer, dimension(nspmd) :: s_index_1,r_index_1,s_index_2,r_index_2,s_index_3,r_index_3
+      integer, dimension(nspmd) :: s_req_1,r_req_1,s_req_2,r_req_2,s_req_3,r_req_3
+      real(kind=WP), dimension(:), allocatable :: s_buffer,r_buffer
+      type(array_type), dimension(:), allocatable :: s_elcenter,r_elcenter
+      type(array_type), dimension(:), allocatable :: s_volfrac,r_volfrac
 C-----------------------------------------------
 C   S o u r c e   L i n e s 
 C-----------------------------------------------
+      s_lesdvois = nesdvois(nspmd+1)
+      s_lercvois = nercvois(nspmd+1)
+      s_fr_elem = iad_elem(1,nspmd+1) - iad_elem(1,1)
+      n_entity = numels+nsvois+numelq+nqvois      
+
       DO NG=ITASK+1,NGROUP,NTHREAD      
 C     ALE ON / OFF
          IF (IPARG(76, NG)  ==  1) CYCLE ! --> OFF
@@ -116,74 +148,95 @@ C     ALE ON / OFF
          IF ((JALE   /=  0) .OR. ((JEUL   /=  0) .AND. (NCYCLE  ==  0 .OR. MCHECK   /=  0))) THEN
             !!!   Volume fraction
             DO I=LFT,LLT 
-               II     = I+NFT    
+              ii = i + nft    
                
-               !!!centroid element
-               ALEMUSCL_Buffer%ELCENTER(II,1) = ZERO ;
-               ALEMUSCL_Buffer%ELCENTER(II,2) = ZERO ;
-               ALEMUSCL_Buffer%ELCENTER(II,3) = ZERO
-               VOL = ZERO
-               !!! Face 1
-               !!! Points 321
-               A = X(1:3, IXS(1+3, II)) ; B = X(1:3, IXS(2+1, II)) ; C = X(1:3, IXS(1+1, II))
-               CALL GEOM(A, B, C,ALEMUSCL_Buffer%ELCENTER(II,1),ALEMUSCL_Buffer%ELCENTER(II,2),ALEMUSCL_Buffer%ELCENTER(II,3), VOL)
-               !!! Points 314
-               A = X(1:3, IXS(3+1, II)) ; B = X(1:3, IXS(1+1, II)) ; C = X(1:3, IXS(4+1, II))
-               CALL GEOM(A, B, C,ALEMUSCL_Buffer%ELCENTER(II,1),ALEMUSCL_Buffer%ELCENTER(II,2),ALEMUSCL_Buffer%ELCENTER(II,3), VOL)
+              !!!centroid element
+              ALEMUSCL_Buffer%ELCENTER(II,1) = ZERO
+              ALEMUSCL_Buffer%ELCENTER(II,2) = ZERO
+              ALEMUSCL_Buffer%ELCENTER(II,3) = ZERO
+              vol = ZERO
 
-               !!! Face 2
-               !!! Points 473
-               A = X(1:3, IXS(4+1, II)) ; B = X(1:3, IXS(7+1, II)) ; C = X(1:3, IXS(3+1, II))
-               CALL GEOM(A, B, C,ALEMUSCL_Buffer%ELCENTER(II,1),ALEMUSCL_Buffer%ELCENTER(II,2),ALEMUSCL_Buffer%ELCENTER(II,3), VOL)
+              node_pos(1:3,1) = x(1:3,ixs(1+1,ii)) ! node 1
+              node_pos(1:3,2) = x(1:3,ixs(1+2,ii)) ! node 2
+              node_pos(1:3,3) = x(1:3,ixs(1+3,ii)) ! node 3
+              node_pos(1:3,4) = x(1:3,ixs(1+4,ii)) ! node 4
+              node_pos(1:3,5) = x(1:3,ixs(1+5,ii)) ! node 5
+              node_pos(1:3,6) = x(1:3,ixs(1+6,ii)) ! node 6
+              node_pos(1:3,7) = x(1:3,ixs(1+7,ii)) ! node 7
+              node_pos(1:3,8) = x(1:3,ixs(1+8,ii)) ! node 8
+              ! face 1 : 321 & 314
+              a(i,1:3,1) = node_pos(1:3,3)           
+              b(i,1:3,1) = node_pos(1:3,2)
+              c(i,1:3,1) = node_pos(1:3,1)
+              a(i,1:3,2) = node_pos(1:3,3)
+              b(i,1:3,2) = node_pos(1:3,1)
+              c(i,1:3,2) = node_pos(1:3,4)
 
-               !!! Points 487
-               A = X(1:3, IXS(4+1, II)) ; B = X(1:3, IXS(8+1, II)) ; C = X(1:3, IXS(7+1, II))
-               CALL GEOM(A, B, C,ALEMUSCL_Buffer%ELCENTER(II,1),ALEMUSCL_Buffer%ELCENTER(II,2),ALEMUSCL_Buffer%ELCENTER(II,3), VOL)
+              ! face 2 : 473 & 487
+              a(i,1:3,3) = node_pos(1:3,4)
+              b(i,1:3,3) = node_pos(1:3,7)
+              c(i,1:3,3) = node_pos(1:3,3)
+              a(i,1:3,4) = node_pos(1:3,4)
+              b(i,1:3,4) = node_pos(1:3,8)
+              c(i,1:3,4) = node_pos(1:3,7)
 
-               !!! Face 3
-               !!! Points 678
-               A = X(1:3, IXS(6+1, II)) ; B = X(1:3, IXS(7+1, II)) ; C = X(1:3, IXS(8+1, II))
-               CALL GEOM(A, B, C,ALEMUSCL_Buffer%ELCENTER(II,1),ALEMUSCL_Buffer%ELCENTER(II,2),ALEMUSCL_Buffer%ELCENTER(II,3), VOL)
+              ! face 3 : 678 & 685
+              a(i,1:3,5) = node_pos(1:3,6)
+              b(i,1:3,5) = node_pos(1:3,7)
+              c(i,1:3,5) = node_pos(1:3,8)
+              a(i,1:3,6) = node_pos(1:3,6)
+              b(i,1:3,6) = node_pos(1:3,8)
+              c(i,1:3,6) = node_pos(1:3,5)
 
-               !!! Points 685
-               A = X(1:3, IXS(6+1, II)) ; B = X(1:3, IXS(8+1, II)) ; C = X(1:3, IXS(5+1, II))
-               CALL GEOM(A, B, C,ALEMUSCL_Buffer%ELCENTER(II,1),ALEMUSCL_Buffer%ELCENTER(II,2),ALEMUSCL_Buffer%ELCENTER(II,3), VOL)
+              ! face 4 : 126 & 165
+              a(i,1:3,7) = node_pos(1:3,1)
+              b(i,1:3,7) = node_pos(1:3,2)
+              c(i,1:3,7) = node_pos(1:3,6)
+              a(i,1:3,8) = node_pos(1:3,1)
+              b(i,1:3,8) = node_pos(1:3,6)
+              c(i,1:3,8) = node_pos(1:3,5)
 
-               !!! Face 4
-               !!! Points 126
-               A = X(1:3, IXS(1+1, II)) ; B = X(1:3, IXS(2+1, II)) ; C = X(1:3, IXS(6+1, II))
-               CALL GEOM(A, B, C,ALEMUSCL_Buffer%ELCENTER(II,1),ALEMUSCL_Buffer%ELCENTER(II,2),ALEMUSCL_Buffer%ELCENTER(II,3), VOL)
+              ! face 5 : 236 & 376
+              a(i,1:3,9)  = node_pos(1:3,2)
+              b(i,1:3,9)  = node_pos(1:3,3)
+              c(i,1:3,9)  = node_pos(1:3,6)
+              a(i,1:3,10) = node_pos(1:3,3)
+              b(i,1:3,10) = node_pos(1:3,7)
+              c(i,1:3,10) = node_pos(1:3,6)
 
-               !!! Points 165
-               A = X(1:3, IXS(1+1, II)) ; B = X(1:3, IXS(6+1, II)) ; C = X(1:3, IXS(5+1, II))
-               CALL GEOM(A, B, C,ALEMUSCL_Buffer%ELCENTER(II,1),ALEMUSCL_Buffer%ELCENTER(II,2),ALEMUSCL_Buffer%ELCENTER(II,3), VOL)
-
-               !!! Face 5
-               !!! Points 236
-               A = X(1:3, IXS(2+1, II)) ; B = X(1:3, IXS(3+1, II)) ; C = X(1:3, IXS(6+1, II))
-               CALL GEOM(A, B, C,ALEMUSCL_Buffer%ELCENTER(II,1),ALEMUSCL_Buffer%ELCENTER(II,2),ALEMUSCL_Buffer%ELCENTER(II,3), VOL)
-
-               !!! Points 376
-               A = X(1:3, IXS(3+1, II)) ; B = X(1:3, IXS(7+1, II)) ; C = X(1:3, IXS(6+1, II))
-               CALL GEOM(A, B, C,ALEMUSCL_Buffer%ELCENTER(II,1),ALEMUSCL_Buffer%ELCENTER(II,2),ALEMUSCL_Buffer%ELCENTER(II,3), VOL)
-
-               !!! Face 6
-               !!! Points 154
-               A = X(1:3, IXS(1+1, II)) ; B = X(1:3, IXS(5+1, II)) ; C = X(1:3, IXS(4+1, II))
-               CALL GEOM(A, B, C,ALEMUSCL_Buffer%ELCENTER(II,1),ALEMUSCL_Buffer%ELCENTER(II,2),ALEMUSCL_Buffer%ELCENTER(II,3), VOL)
-
-               !!! Points 458
-               A = X(1:3, IXS(4+1, II)) ; B = X(1:3, IXS(5+1, II)) ; C = X(1:3, IXS(8+1, II))
-               CALL GEOM(A, B, C,ALEMUSCL_Buffer%ELCENTER(II,1),ALEMUSCL_Buffer%ELCENTER(II,2),ALEMUSCL_Buffer%ELCENTER(II,3), VOL)
-
-
-               VOL = VOL / 6.D0
-               ALEMUSCL_Buffer%ELCENTER(II,1:3) = ALEMUSCL_Buffer%ELCENTER(II,1:3) / 12.D0 / VOL
-               
-               
-            ENDDO  
+              ! face 6 : 154 & 458
+              a(i,1:3,11) = node_pos(1:3,1)
+              b(i,1:3,11) = node_pos(1:3,5)
+              c(i,1:3,11) = node_pos(1:3,4)
+              a(i,1:3,12) = node_pos(1:3,4)
+              b(i,1:3,12) = node_pos(1:3,5)
+              c(i,1:3,12) = node_pos(1:3,8)
+            ENDDO
+            nel = llt - lft + 1 
+            call geom_vec(nel,nft,n_entity,a,b,c,alemuscl_buffer%elcenter)
          ENDIF
       ENDDO  ! NG=ITASK+1,NGROUP,NTHREAD
+
+      CALL MY_BARRIER   
+      ! ------------
+      ! MPI comms : iSend & iRecv Centroid coordinates
+      if(nspmd>1) then
+        if(itask==0) call startime(timers,timer_spmdcfd)
+        if(itask==0) then
+          flag = 0
+          s_proc_nb_1 = 0
+          r_proc_nb_1 = 0
+          allocate(s_elcenter(nspmd))
+          allocate(r_elcenter(nspmd))
+          call spmd_exch_n_neighbor(flag,nspmd,n_entity,3,s_lesdvois,s_lercvois, 
+     .                              s_proc_nb_1,r_proc_nb_1,s_index_1,r_index_1,s_req_1,r_req_1, 
+     .                              nesdvois,nercvois,lesdvois,lercvois, 
+     .                              alemuscl_buffer%elcenter,s_elcenter,r_elcenter)
+        endif
+         if(itask==0) call stoptime(timers,timer_spmdcfd)
+      endif
+      ! ------------
+
       DO NG=ITASK+1,NGROUP,NTHREAD      
 C     ALE ON / OFF
          IF (IPARG(76, NG)  ==  1) CYCLE ! --> OFF
@@ -217,28 +270,51 @@ C     ALE ON / OFF
       ENDDO  ! NG=ITASK+1,NGROUP,NTHREAD
 
       CALL MY_BARRIER
-      
-      !!! MPI Comm
-      IF(NSPMD  > 1)THEN
-!$OMP SINGLE
-         !!! Volumic fractions comm
-         DO ITRIMAT = 1, TRIMAT
-            CALL SPMD_E1VOIS(ALEMUSCL_Buffer%VOLUME_FRACTION(1,ITRIMAT), NERCVOIS, NESDVOIS,
-     .           LERCVOIS, LESDVOIS, LENCOM)
-         ENDDO
-         !!! Centroid coordinates comm
-         DO JJ = 1, 3
-            CALL SPMD_E1VOIS(ALEMUSCL_Buffer%ELCENTER(1,JJ), NERCVOIS, NESDVOIS,
-     .           LERCVOIS, LESDVOIS, LENCOM)
-         ENDDO
-!$OMP END SINGLE
-      ENDIF
+      ! ------------
+      ! MPI comms : Wait & Finalize Centroid coordinates + blocking mpi comms for fraction of volume
+      if(nspmd>1) then
+        if(itask==0) call startime(timers,timer_spmdcfd)
+        if(itask==0) then
+          ! wait & Finalize Centroid coordinates 
+          flag = 1
+          call spmd_exch_n_neighbor(flag,nspmd,n_entity,3,s_lesdvois,s_lercvois, 
+     .                              s_proc_nb_1,r_proc_nb_1,s_index_1,r_index_1,s_req_1,r_req_1, 
+     .                              nesdvois,nercvois,lesdvois,lercvois, 
+     .                              alemuscl_buffer%elcenter,s_elcenter,r_elcenter)
+          deallocate(s_elcenter)
+          deallocate(r_elcenter)
+
+          ! iSend & iRecv fraction of volume
+          flag = 0
+          s_proc_nb_2 = 0
+          r_proc_nb_2 = 0
+          allocate(s_volfrac(nspmd))
+          allocate(r_volfrac(nspmd))
+          call spmd_exch_n_neighbor(flag,nspmd,n_entity,TRIMAT,s_lesdvois,s_lercvois, 
+     .                              s_proc_nb_2,r_proc_nb_2,s_index_2,r_index_2,s_req_2,r_req_2, 
+     .                              nesdvois,nercvois,lesdvois,lercvois, 
+     .                              alemuscl_buffer%volume_fraction,s_volfrac,r_volfrac)
+          ! wait & Finalize fraction of volume
+          flag = 1
+          call spmd_exch_n_neighbor(flag,nspmd,n_entity,TRIMAT,s_lesdvois,s_lercvois,
+     .                              s_proc_nb_2,r_proc_nb_2,s_index_2,r_index_2,s_req_2,r_req_2,
+     .                              nesdvois,nercvois,lesdvois,lercvois,
+     .                              alemuscl_buffer%volume_fraction,s_volfrac,r_volfrac)
+          deallocate(s_volfrac)
+          deallocate(r_volfrac) 
+        endif
+         if(itask==0) call stoptime(timers,timer_spmdcfd)
+      endif
+      ! ------------      
+
       CALL MY_BARRIER 
       
       FIRST = 1 + ITASK * NUMNOD / NTHREAD
       LAST = (1 + ITASK) * NUMNOD / NTHREAD
-      ALEMUSCL_Buffer%NODE_MAX_VALUE(FIRST:LAST,1:TRIMAT) = -EP30
-      ALEMUSCL_Buffer%NODE_MIN_VALUE(FIRST:LAST,1:TRIMAT) = EP30
+      do itrimat=1,trimat
+        ALEMUSCL_Buffer%NODE_MAX_VALUE(FIRST:LAST,itrimat) = -EP30
+        ALEMUSCL_Buffer%NODE_MIN_VALUE(FIRST:LAST,itrimat) = EP30
+      enddo
       DO ITRIMAT = 1, TRIMAT
         DO NODE_ID = FIRST,LAST
             JMIN = ALEMUSCL_Buffer%pADDCNEL(NODE_ID)
@@ -255,18 +331,28 @@ C     ALE ON / OFF
          ENDDO
       ENDDO
       CALL MY_BARRIER 
-       !!! MPI Comm
-      IF(NSPMD  > 1)THEN
-!$OMP SINGLE
-         DO ITRIMAT = 1, TRIMAT
-            CALL  SPMD_EXCH_MIN_MAX(IAD_ELEM ,FR_ELEM ,
-     .                              ALEMUSCL_Buffer%NODE_MIN_VALUE(1,ITRIMAT), ALEMUSCL_Buffer%NODE_MAX_VALUE(1,ITRIMAT) )
-         ENDDO
-!$OMP END SINGLE
-      ENDIF
-      CALL MY_BARRIER
+      ! ------------
+      ! MPI comms : iSend & iRecv min/max
+      if(nspmd>1) then
+        if(itask==0) call startime(timers,timer_spmdcfd)
+        if(itask==0) then
+          flag = 0
+          s_proc_nb_3 = 0
+          r_proc_nb_3 = 0
+          buff_size = iad_elem(1,nspmd+1)
+          allocate(s_buffer(2*buff_size*3))
+          allocate(r_buffer(2*buff_size*3))
+          call spmd_exch_min_max(flag,nspmd,numnod,buff_size,s_proc_nb_3,r_proc_nb_3,             
+     .                           s_fr_elem,s_index_3,r_index_3,s_req_3,r_req_3,fr_elem,iad_elem,            
+     .                           alemuscl_buffer%node_min_value,alemuscl_buffer%node_max_value,s_buffer,r_buffer)
+        endif
+         if(itask==0) call stoptime(timers,timer_spmdcfd)
+      endif
+      ! ------------     
 
-      DO NG=ITASK+1,NGROUP,NTHREAD  
+
+!$omp do schedule(guided)
+      DO NG=1,ngroup
 C     ALE ON / OFF
          IF (IPARG(76, NG)  ==  1) CYCLE ! --> OFF
          CALL INITBUF(IPARG    ,NG      ,                  
@@ -279,23 +365,45 @@ C     ALE ON / OFF
          IF(JALE+JEUL == 0)    CYCLE
          IF(IPARG(8,NG) == 1)  CYCLE
          IF(IPARG(1,NG)  /= 51) CYCLE
+         isolnod = iparg(28,ng)
+         facet_nb = 6
+         if(ity==1.and.isolnod==4) facet_nb = 4
+         if(ity==2) facet_nb = 4
          LFT = 1
          !!!   Reconstruct gradient
-         DO ITRIMAT = 1, TRIMAT
-            CALL GRADIENT_RECONSTRUCTION( IXS, X, ALE_CONNECT, ITRIMAT, SEGVAR)
-         ENDDO  !  ITRIMAT = 1, TRIMAT     
-      END DO  !  NG=ITASK+1,NGROUP,NTHREAD 
+         nel = llt - lft + 1 
+         call gradient_reconstruction_new(numels,numnod,nel,nft,trimat,facet_nb,ixs,x,ale_connect,segvar)
+      END DO
+!$omp end do      
 
+      CALL MY_BARRIER
+
+      ! ------------
+      ! MPI comms : Wait & Finalize min/max
+      if(nspmd>1) then
+        if(itask==0) call startime(timers,timer_spmdcfd)
+        if(itask==0) then
+          flag = 1
+          buff_size = iad_elem(1,nspmd+1)          
+          call spmd_exch_min_max(flag,nspmd,numnod,buff_size,s_proc_nb_3,r_proc_nb_3,             
+     .                           s_fr_elem,s_index_3,r_index_3,s_req_3,r_req_3,fr_elem,iad_elem,            
+     .                           alemuscl_buffer%node_min_value,alemuscl_buffer%node_max_value,s_buffer,r_buffer)
+          deallocate(s_buffer)
+          deallocate(r_buffer)
+        endif
+         if(itask==0) call stoptime(timers,timer_spmdcfd)
+      endif
+      ! ------------
       CALL MY_BARRIER
 
       IF (NSPMD   >  1) THEN
 C     MPI Communication for gradients
-!$OMP SINGLE
-         DO ITRIMAT = 1, TRIMAT
-            CALL SPMD_EXCHANGE_GRAD(3, NUMELS + NSVOIS + NUMELQ + NQVOIS,3,ALEMUSCL_Buffer%GRAD(1,1,ITRIMAT), 
-     .           NERCVOIS, NESDVOIS, LERCVOIS, LESDVOIS, LENCOM)
-         ENDDO
-!$OMP END SINGLE         
+        if(itask==0) call startime(timers,timer_spmdcfd)
+        if(itask==0) then
+          call spmd_exchange_grad(n_entity,3,TRIMAT,alemuscl_buffer%grad,
+     .                            nercvois,nesdvois,lercvois,lesdvois,lencom)
+        endif
+        if(itask==0) call stoptime(timers,timer_spmdcfd)
       ENDIF
       CALL MY_BARRIER
       
@@ -320,4 +428,3 @@ C     ALE ON / OFF
  
 C-----------------------------------------------      
       END SUBROUTINE ALE51_GRADIENT_RECONSTRUCTION
-      

commit 2d65d8001e48a6c942a77abe532cea71e204315b

ALE : fix an issue introduced by fa99589d7a990a023f2accc468f2003ac2d029c9 - 2d & nmult>0

engine/source/ale/ale51/ale51_init.F
@@ -53,12 +53,6 @@ C-----------------------------------------------
       USE I22TRI_MOD           
       USE ALE_CONNECTIVITY_MOD
       use element_mod , only : nixs
-
-
-
-
-
-      USE DEBUG_MOD
 C-----------------------------------------------
 C   I m p l i c i t   T y p e s
 C-----------------------------------------------

engine/source/ale/alemain.F
@@ -147,9 +147,6 @@ C-----------------------------------------------
       use bcs_wall_trigger_mod
       use glob_therm_mod
       use element_mod , only : nixs,nixq,nixtg,nixt
-
-
-      use debug_mod
 C-----------------------------------------------
 C   I m p l i c i t   T y p e s
 C-----------------------------------------------

engine/source/ale/alemuscl/ale51_gradient_reconstruction.F
@@ -80,10 +80,6 @@ C-----------------------------------------------
       use geom_vec_mod , only : geom_vec
       use spmd_exch_n_neighbor_mod , only : spmd_exch_n_neighbor
       use array_mod
-
-
-
-      use debug_mod
 C-----------------------------------------------
 C   I m p l i c i t   T y p e s
 C-----------------------------------------------

engine/source/ale/alemuscl/geom_vec.F90
@@ -39,7 +39,6 @@
 !||    ale51_gradient_reconstruction   ../engine/source/ale/alemuscl/ale51_gradient_reconstruction.F
 !||--- uses       -----------------------------------------------------
 !||    constant_mod                    ../common_source/modules/constant_mod.F
-!||    debug_mod                       ../engine/share/modules/debug_mod.F
 !||    mvsiz_mod                       ../engine/share/spe_inc/mvsiz_mod.F90
 !||    precision_mod                   ../common_source/modules/precision_mod.F90
 !||====================================================================
@@ -50,10 +49,6 @@
           use constant_mod, only : half,zero
           use precision_mod, only : WP
           use mvsiz_mod, only : MVSIZ
-
-
-
-          USE DEBUG_MOD
 ! ----------------------------------------------------------------------------------------------------------------------
 !                                                   Implicit none
 ! ----------------------------------------------------------------------------------------------------------------------

engine/source/ale/bimat/amulf2.F
@@ -167,12 +167,13 @@ C
        ENDDO !next I
 
        DO I=LFT,LLT
-         II=I+(JM-1)*NUMELQ
+         II=I+nft+(JM-1)*NUMELQ
          FLUX(II,1)=FLUB1(I)-UPW(I)*ABS(FLUB1(I))
          FLUX(II,2)=FLUB2(I)-UPW(I)*ABS(FLUB2(I))
          FLUX(II,3)=FLUB3(I)-UPW(I)*ABS(FLUB3(I))
          FLUX(II,4)=FLUB4(I)-UPW(I)*ABS(FLUB4(I))
 
+         II=I+(JM-1)*NUMELQ
          FLU1(II) =FLUB1(I)+UPW(I)*ABS(FLUB1(I))
      .            +FLUB2(I)+UPW(I)*ABS(FLUB2(I))
      .            +FLUB3(I)+UPW(I)*ABS(FLUB3(I))

engine/source/mpi/ale/spmd_exch_min_max.F90
@@ -131,10 +131,6 @@
               if (s_size > 0) then
                 do j=iad_elem(1,p),iad_elem(1,p+1)-1
                   node_id = fr_elem(j)
-                  if(s_address_2>2*lencom*3) then
-                    print*," aaaarf !",s_address_2,lencom,iad_elem(1,nspmd+1)
-                    stop
-                  endif
                   s_buffer(s_address_2) = min_value(node_id,1)
                   s_buffer(s_address_2+1) = max_value(node_id,1)
                   s_buffer(s_address_2+2) = min_value(node_id,2)